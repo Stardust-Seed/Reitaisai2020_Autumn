9ecb8e30bdf004b37d1762fccdd9bc5af633d036
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-07-21 08:45:37 +0900

    [update]微修正

--- Source/BaseScene.h
+++ Source/BaseScene.h
@@ -1,19 +1,17 @@
 #ifndef _BaseScene_H
 #define _BaseScene_H_
 
-
 enum EBaseScene
 {
-	Title = 0,				//タイトル画面
-	Menu = 1,				//メニュー画面
-	Game = 2,				//ゲーム画面
-	Claer = 3,				//クリア画面
-	Gameover = 4,			//ゲームオブジェクト
-	Option = 5,				//オプション
-	Pausemenu = 6,			//ポーズメニュー
-	Charaselect = 7,		//キャラ選択画面
-	Levelselect = 8,		//難易度選択画面
-	
+	Title,				//タイトル画面
+	Menu,				//メニュー画面
+	Game,				//ゲーム画面
+	Claer,				//クリア画面
+	Gameover,			//ゲームオブジェクト
+	Option,				//オプション
+	Pausemenu,			//ポーズメニュー
+	Charaselect,		//キャラ選択画面
+	Levelselect,		//難易度選択画面
 };
 
 class BaseScene
@@ -23,4 +21,5 @@ public:
 
 	virtual void Update() = 0;		//更新処理
 };
+
 #endif
