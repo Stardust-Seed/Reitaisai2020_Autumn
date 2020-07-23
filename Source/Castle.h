253e0d9fc745c0c8dbba7eff1d9e450c667c843a
澤村遼 <ryo882514@gmail.com>
2020-07-23 13:43:59 +0900

    [update]座標の変数とゲッターセッターを追加

--- Source/Castle.h
+++ Source/Castle.h
@@ -17,18 +17,32 @@ class Castle:Object{
 private:
 
 	int Durability;	//耐久力
+	int	x;	        //拠点のx
+    int	y;	        //拠点のy
+    int	width;  	//拠点のwidth
+    int	height; 	//拠点のheight
 
 	bool isActive;	//拠点の生存フラグ
 
 public:
 
-	 virtual bool ClisionHit(float mx, float my, float mw, float mh,
-		     float ox, float oy, float ow, float oh) = 0;
+	 bool ClisionHit(float mx, float my, float mw, float mh,
+		     float ox, float oy, float ow, float oh);
 	 //m = my 自分   o = opnet 相手
 
-	 virtual void Update();                //更新処理
-	 virtual void Draw();                  //描画処理
-	 virtual void EnemyHit(int _power);    //敵が衝突してきたら実行される関数
+	 //void Update(Enemy& enemy);    //更新処理
+	 void Update();                  //更新処理
+	 void Draw();                    //描画処理
+
+	 void Set_x(int _x) { x = _x; }                        //セッター
+	 void Set_y(int _y) { y = _y; }                        //セッター
+	 void Set_width(int _width) { width = _width; }        //セッター
+	 void Set_height(int _height) { height = _height; }    //セッター
+
+	 int Get_x() { return x; }                             //ゲッター
+	 int Get_y() { return y; }                             //ゲッター
+	 int Get_width() { return width; }                     //ゲッター
+	 int Get_height() { return height; }                   //ゲッター
 
 };
 
