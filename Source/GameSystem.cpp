633815fa284a940b6e1d2eb37cae70b7d9efa856
kuroneko96 <kaityou5555@gmail.com>
2020-07-27 03:53:21 +0900

    [update]画面解像度を変更((1280,720)→(1024,768))

--- Source/GameSystem.cpp
+++ Source/GameSystem.cpp
@@ -7,7 +7,7 @@ bool GameSystem::Init() {
 	SetOutApplicationLogValidFlag(FALSE);	//ログ出力を行わない
 	ChangeWindowMode(TRUE);					//ウィンドウモードに変更
 	SetMainWindowText("東方河本録");		//ウィンドウのタイトルを変更する
-	SetGraphMode(1280, 720, 32);			//画面の解像度変更(マジックナンバーは後で消す)
+	SetGraphMode(1024, 768, 32);			//画面の解像度変更(マジックナンバーは後で消す)
 	if (DxLib_Init()) {						//DXライブラリ初期化処理
 		return false;
 	}
