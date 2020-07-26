02656b8a7754cc526c4123dee4febf3ce1da5812
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-07-27 03:25:23 +0900

    [add]Menu.h、Menu.cpp追加　[update]BaseScene.hを少しだけ変更

---
+++ Source/Menu.h
@@ -0,0 +1,30 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#include "BaseScene.h"
+
+const static int GAME_Y = 240;		//「ゲーム」文字のy位置
+const static int CONFIG_Y = 270;    //「設定」文字のy位置
+
+enum eMenutype
+{
+	eMenutype_Menu,		//メニュー
+	eMenutype_Game,		//ゲーム画面
+	eMenutype_Option,	//オプション
+	eMenutype_Num,		//項目の数
+};
+
+class Menu:public virtual BaseScene
+{
+private:
+	int NowSelect; 
+
+public:
+	void SelectMenu();					//メニュー項目の選択
+
+	virtual void Update();				//更新処理
+
+	virtual void Draw();				//描画処理
+};
+
+#endif
