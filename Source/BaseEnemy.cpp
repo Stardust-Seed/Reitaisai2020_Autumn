3b1d86be9c661985baa970510d53338eb90c3d62
kuroneko96 <kaityou5555@gmail.com>
2020-07-27 03:50:04 +0900

    [add]敵の共通処理を追加

--- Source/BaseEnemy.cpp
+++ Source/BaseEnemy.cpp
@@ -1,56 +1,117 @@
+#include <DxLib.h>
 #include "BaseEnemy.h"
 
-const float BaseEnemy::ENEMY_SPAWNXLEFT = 0;
-const float BaseEnemy::ENEMY_SPAWNYLEFT = 0;
-const float BaseEnemy::ENEMY_SPAWNXUP = 0;
-const float BaseEnemy::ENEMY_SPAWNYUP = 0;
-const float BaseEnemy::ENEMY_SPAWNXRIGHT = 0;
-const float BaseEnemy::ENEMY_SPAWNYRIGHT = 0;
-const float BaseEnemy::ENEMY_SPAWNXDOWN = 0;
-const float BaseEnemy::ENEMY_SPAWNYDOWN = 0;
+const float BaseEnemy::ENEMY_SPAWNXLEFT = 82;
+const float BaseEnemy::ENEMY_SPAWNYLEFT = 360;
+const float BaseEnemy::ENEMY_SPAWNXUP = 488;
+const float BaseEnemy::ENEMY_SPAWNYUP = -48;
+const float BaseEnemy::ENEMY_SPAWNXRIGHT = 894;
+const float BaseEnemy::ENEMY_SPAWNYRIGHT = 360;
+const float BaseEnemy::ENEMY_SPAWNXDOWN = 488;
+const float BaseEnemy::ENEMY_SPAWNYDOWN = 816;
 const int BaseEnemy::DIRECTIONLEFT = 0;
 const int BaseEnemy::DIRECTIONRIGHT = 1;
 const int BaseEnemy::DIRECTIONUP = 2;
 const int BaseEnemy::DIRECTIONDOWN = 3;
 
-//コンストラクタ
-BaseEnemy::BaseEnemy() {
-	speed = 0;
-	power = 0;
-	dulability = 0;
-	direction = 0;
+BaseEnemy::BaseEnemy(float _speed, float _power, int _durability, int _direction) {
+	speed = _speed;
+	power = _power;
+	durability = _durability;
+	direction = _direction;
+
+	if (direction == DIRECTIONLEFT) {
+		x = ENEMY_SPAWNXLEFT;
+		y = ENEMY_SPAWNYLEFT;
+	}
+
+	if (direction == DIRECTIONRIGHT) {
+		x = ENEMY_SPAWNXRIGHT;
+		y = ENEMY_SPAWNYRIGHT;
+	}
+
+	if (direction == DIRECTIONUP) {
+		x = ENEMY_SPAWNXUP;
+		y = ENEMY_SPAWNYUP;
+	}
+
+	if (direction == DIRECTIONDOWN) {
+		x = ENEMY_SPAWNXDOWN;
+		y = ENEMY_SPAWNYDOWN;
+	}
+
+	width = 48;
+	height = 48;
+
 	isActive = true;
 	isAttack = false;
 }
 
 //移動処理
 void BaseEnemy::Move() {
-	switch (direction) {
-	case BaseEnemy::DIRECTIONLEFT:
+
+	if (direction == BaseEnemy::DIRECTIONLEFT) {
 		x += speed;
-		break;
-	case BaseEnemy::DIRECTIONRIGHT:
-		x -= speed;
-		break;
-	case BaseEnemy::DIRECTIONUP:
-		y += speed;
-		break;
-	case BaseEnemy::DIRECTIONDOWN:
-		y -= speed;
-		break;
 	}
+
+	if (direction == BaseEnemy::DIRECTIONRIGHT) {
+		x -= speed;
+	}
+
+	if (direction == BaseEnemy::DIRECTIONUP) {
+		y += speed;
+	}
+
+	if (direction == BaseEnemy::DIRECTIONDOWN) {
+		y -= speed;
+	}
+
 }
 
 //生存判定処理
 void BaseEnemy::JudgeActive() {
-	if (dulability >= 0) {
+	if (durability >= 0) {
 		isActive = false;
 	}
 }
 
+void BaseEnemy::SearchPlayer(int _px, int _py, int _pw, int _ph) {
+
+	if (direction == BaseEnemy::DIRECTIONLEFT) {
+		if (_px + _pw >= x + width && _px <= x + width && _py + _ph >= y && _py <= y + height) {
+			isAttack = true;
+		}
+	}
+
+	if (direction == BaseEnemy::DIRECTIONRIGHT) {
+		if (_px + _pw >= x - width && _px <= x - width && _py + _ph >= y && _py <= y + height) {
+			isAttack = true;
+		}
+	}
+
+	if (direction == BaseEnemy::DIRECTIONUP) {
+		if (_px + _pw >= x && _px <= x + width && _py + _ph >= y + height && _py <= y + height) {
+			isAttack = true;
+		}
+	}
+
+	if (direction == BaseEnemy::DIRECTIONDOWN) {
+		if (_px + _pw >= x && _px <= x + width && _py + _ph >= y - height && _py <= y - height) {
+			isAttack = true;
+		}
+	}
+}
+
+void BaseEnemy::SearchCastle(int _ox, int _oy, int _ow, int _oh) {
+	if (x + width >= _ox && x <= _ox + _ow && y + height >= _oy && y <= _oy + _oh) {
+		isAttack = true;
+	}
+	isActive = false;
+}
+
 //当たり判定処理
 bool BaseEnemy::ClisionHit(float mx, float my, float mw, float mh,
-	float ox, float oy, float ow, float oh){
+	float ox, float oy, float ow, float oh) {
 
 	if (mx + mw >= ox && mx <= ox + ow && my + mh >= oy && my <= oy + oh) {
 		return true;
