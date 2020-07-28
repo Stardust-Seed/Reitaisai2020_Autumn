7ee86f76641e19bf47cdfe14a56af39d7838c676
kuroneko96 <kaityou5555@gmail.com>
2020-07-29 02:06:37 +0900

    [update]画面解像度のマジックナンバーを定数に変更

--- Source/GameSystem.cpp
+++ Source/GameSystem.cpp
@@ -1,17 +1,18 @@
 #include <DxLib.h>
 #include "GameSystem.h"
 
+#include "Define.h"
 #include "GameMain.h"
 
 bool GameSystem::Init() {
-	SetOutApplicationLogValidFlag(FALSE);	//ログ出力を行わない
-	ChangeWindowMode(TRUE);					//ウィンドウモードに変更
-	SetMainWindowText("東方河本録");		//ウィンドウのタイトルを変更する
-	SetGraphMode(1024, 768, 32);			//画面の解像度変更(マジックナンバーは後で消す)
-	if (DxLib_Init()) {						//DXライブラリ初期化処理
+	SetOutApplicationLogValidFlag(FALSE);					//ログ出力を行わない
+	ChangeWindowMode(TRUE);									//ウィンドウモードに変更
+	SetMainWindowText("東方河本録");						//ウィンドウのタイトルを変更する
+	SetGraphMode(GAME_WIDTH, GAME_HEIHGT, GAME_COLORBIT);	//画面の解像度変更
+	if (DxLib_Init()) {										//DXライブラリ初期化処理
 		return false;
 	}
-	SetDrawScreen(DX_SCREEN_BACK);			//裏画面処理を設定する
+	SetDrawScreen(DX_SCREEN_BACK);							//裏画面処理を設定する
 
 	return true;
 }
