3394ac952a998be1c36a842582cb403201d8a2fe
澤村遼 <ryo882514@gmail.com>
2020-07-31 01:15:51 +0900

    [fix]x,y,width,heightがint型にされてるのでfloat型に修正

--- Source/Castle.h
+++ Source/Castle.h
@@ -11,7 +11,7 @@
 呼ばれ先      :
 詳細説明	  : 拠点
 --------------------------------------------------------------*/
-
+class Fairy_Speed;
 class Castle:public virtual Object{
 
 private:
@@ -27,16 +27,16 @@ public:
 		     float ox, float oy, float ow, float oh);
 	 //m = my 自分   o = opnet 相手
 
-	 void Update();                //更新処理
+	 void Update(Fairy_Speed* enemy);                //更新処理
 	 void Draw();                  //描画処理
 	 void HitDamage();             //ダメージ処理
 	 void JudgeActive();           //生存判定処理
 
-	 void Set_x(int _x) { x = _x; }                                 //セッター
-	 void Set_y(int _y) { y = _y; }                                 //セッター
-	 void Set_width(int _width) { width = _width; }                 //セッター
-	 void Set_height(int _height) { height = _height; }             //セッター
-	 void Set_IsActive(bool _isActive) { isActive = _isActive; }    //セッター
+	 void Set_x(float _x) { x = _x; }                                 //セッター
+	 void Set_y(float _y) { y = _y; }                                 //セッター
+	 void Set_width(float _width) { width = _width; }                 //セッター
+	 void Set_height(float _height) { height = _height; }             //セッター
+	 void Set_IsActive(bool _isActive) { isActive = _isActive; }      //セッター
 
 	 int Get_x() { return x; }                                      //ゲッター
 	 int Get_y() { return y; }                                      //ゲッター
