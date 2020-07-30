226445c498f177a5550344ef8bbd4e664113e010
dooooori <binamu1215@gmail.com>
2020-07-31 03:52:34 +0900

    [update]コメント追加

--- Source/EnemyManager.cpp
+++ Source/EnemyManager.cpp
@@ -9,31 +9,31 @@ EnemyManager::EnemyManager() {
 
 EnemyManager::~EnemyManager() {
 	for (int num = 0; num < ENEMY_NUM; num++) {
-		delete Fairy_E[num];		//
+		delete Fairy_E[num];		//デリート処理
 	}
 }
 void EnemyManager::SpawnEnemy() {
 	for (int num = 0; num < ENEMY_NUM; num++) {
-		if (Fairy_E[num] == NULL) {
-			Fairy_E[num] = new Fairy_Endurance;
-			break;
+		if (Fairy_E[num] == NULL) {				//NULLの場合
+			Fairy_E[num] = new Fairy_Endurance;	//生成処理
+			break;								//一体生成したら抜ける
 		}
 	}
 }
 
 void EnemyManager::Update(){
-	SpawnEnemy();
+	SpawnEnemy();						//生成呼び出し
 
 	for (int num = 0; num < ENEMY_NUM; num++) {
 		if (Fairy_E[num] != NULL) {		//NULLでない場合
 			/*
-			Fairy_E[num]->Update();		//更新、引数がわからん
+			Fairy_E[num]->Update();		//更新処理、引数がわからん
 			
 			if (Fairy_E[num]->JudgeActive() == false) {
 			//BaseEnemyにアクティブ状態を返す関数が欲しい
 
-			delete Fairy_E[num];
-			Fairy_E[num] = NULL;
+			delete Fairy_E[num];		//アクティブでない場合デリートして
+			Fairy_E[num] = NULL;		//NULLを入れる
 			}
 			*/
 		}
@@ -67,7 +67,7 @@ void EnemyManager::Set_height(int _height) {
 int EnemyManager::Get_ActiveFlg(bool) {
 	for (int num = 0; num < ENEMY_NUM; num++) {
 		if (Fairy_E[num] != NULL) {
-		//	return Fairy_E[num]->();
+		//	return Fairy_E[num]->();	//返す関数が欲しい
 		}
 	}
 }
