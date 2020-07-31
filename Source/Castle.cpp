3e5d44c7be610816bcabf79e9a879d16d152ba92
澤村遼 <ryo882514@gmail.com>
2020-07-31 15:44:00 +0900

    [update]スピード型ではあるけどそいつの値参照してCastle側の処理が動くようになったロジ              あと現状使ってない関数コメント化しといた

--- Source/Castle.cpp
+++ Source/Castle.cpp
@@ -1,6 +1,6 @@
 #include "Castle.h"
 #include "DxLib.h"
-
+#include "Fairy_Speed.h"
 //拠点の耐久力を設定
 const int Castle::MAX_DURABILITY = 100;
 
@@ -9,34 +9,42 @@ Castle::Castle(){
 
 	durability = MAX_DURABILITY;
 	isActive = true;
-	x = 1024 / 2 - 60;
-	y = 768 / 2 - 60;
-	width = x + 60 * 2;
-	height = y + 60 * 2;
+	//テスト用に少しずらしてある
+	x = 1024 / 2 - 60+80;
+	y = 768 / 2 - 60+80;
+	width = 120;
+	height = 120;
 }
+
 //void Castle::Update(Enemy& enemy)constになる予定
-void Castle::Update()
+//今はスピード型の奴参照してテストしてます
+void Castle::Update(Fairy_Speed* enemy)
 {
 	Draw();
-	JudgeActive();
-	HitDamage();
-	//後々こんな感じの記述になる？
-	//ClisionHit(x,y,width,height, enemy.x, enemy.y, enemy.width, enemy.height);
+	ClisionHit(enemy->Get_X(), enemy->Get_Y(), enemy->Get_Width(), enemy->Get_Height(),enemy->GetPower());
 }
 //拠点の描画
 void Castle::Draw()
 {
+	DrawFormatString(10, 100, GetColor(255, 255, 255), "%d", durability);
+	if (isActive == false)
+	{
+		DrawFormatString(10, 130, GetColor(255, 255, 255), "クラムボンは死んだよ");
+	}
+	else
+	{
+		DrawFormatString(10, 130, GetColor(255, 255, 255), "クラムボンはいきてる");
+	}
 	//プレイヤーの移動範囲
-	DrawBox(x-60, y-60, width+60, height+60, GetColor(25, 25, 25), true);
+	DrawBox(x-60, y-60, x+width+60, y+height+60, GetColor(25, 25, 25), true);
 	//拠点の画像を読み込んで描画させます今は四角を表示
-	DrawBox(x, y, width, height, GetColor(255, 255, 255), true);
+	DrawBox(x, y, x+width, y+height, GetColor(255, 255, 255), true);
 }
-
+/*
 //ClisionHitが使えるなら必要なし？
 //拠点が存在しているか判定する処理
 void Castle::JudgeActive()
 {
-	DrawFormatString(10, 100, GetColor(255, 255, 255), "%d",durability);
 	if (durability <= 0)
 	{
 		isActive = false;
@@ -55,24 +63,32 @@ void Castle::HitDamage()
 	//エネミー側待ちなので仮の処理
 	//Durability -= Enemy->GetPower();
 }
+*/
 
 //拠点がダメージを受けたときの処理
-bool Castle::ClisionHit(float mx, float my, float mw, float mh,
-	float ox, float oy, float ow, float oh)
+bool Castle::ClisionHit(float ox, float oy, float ow, float oh,int pow)
 {
-	if (mx + mw >= ox && mx <= ox + ow &&
-		my + mh >= oy && my <= oy + oh)
+	DrawFormatString(10, 500, GetColor(255, 255, 255), "%f", ox);
+	DrawFormatString(10, 520, GetColor(255, 255, 255), "%f", oy);
+	DrawFormatString(10, 540, GetColor(255, 255, 255), "%f", ow);
+	DrawFormatString(10, 560, GetColor(255, 255, 255), "%f", oh);
+	DrawFormatString(10, 580, GetColor(255, 255, 255), "%d", pow);
+
+	if (x + width >= ox && x <= ox + ow &&
+		y + height >= oy && y <= oy + oh)
 	{
-		durability -= 10;    //とりあえずの記述
-		//Durability -= Enemy.GetPower();
+		//durability -= 10;    //とりあえずの記述
+		durability -= pow;
 	}
 
-	if (durability <= 0)
+	if (durability >= 0)
 	{
-		return true;
+		isActive = true;
 	}
 	else
 	{
-		return false;
+		isActive = false;
 	}
+
+	return isActive;
 }
