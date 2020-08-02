ecbb04ed7683a2e1fec38ecf6dbeb04ea6cfc111
kuroneko96 <kaityou5555@gmail.com>
2020-08-03 03:02:13 +0900

    [update]サーチ関連の処理を追加する

--- Source/Fairy_Endurance.cpp
+++ Source/Fairy_Endurance.cpp
@@ -1,15 +1,19 @@
 #include <DxLib.h>
 #include "Fairy_Endurance.h"
 
+#include "BasePlayer.h"
+#include "Castle.h"
+
 Fairy_Endurance::Fairy_Endurance(float _speed, float _power, int _durability, int _direction)
 	:BaseEnemy(_speed, _power, _durability, _direction) {
 
 }
 
-void Fairy_Endurance::Update(Castle* _castle) {
+void Fairy_Endurance::Update(Castle* _castle, BasePlayer* _player) {
 	JudgeActive();
 
-	SearchCastle(392, 264, 240, 240);
+	SearchPlayer(_player->Get_x(), _player->Get_y(), _player->Get_width(), _player->Get_height());
+	SearchCastle(_castle->Get_x(), _castle->Get_y(), _castle->Get_width(), _castle->Get_height());
 
 	Move();
 }
