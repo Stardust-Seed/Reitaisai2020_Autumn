492cbb43ea15793221057c6060aa47de06953fbb
澤村遼 <ryo882514@gmail.com>
2020-08-06 00:20:50 +0900

    [update]ちょろっと追加 マジックナンバーありありロジカルマジカル

--- Source/Title.cpp
+++ Source/Title.cpp
@@ -10,6 +10,7 @@ float o;
 int textSpeed[3];
 int cnt;
 float w;
+
 //コンストラクタ
 Title::Title(){
 
@@ -33,21 +34,24 @@ void Title::Update()
 		DrawFormatString(0,40, GetColor(255, 255, 255), "スペースが押されたのでシーン切り替えされたい");
 		//SceneChange->onSceneChanged(Scene::MENU);みたいな感じかもしれないきっと
 	}
+
 	//却下されれば消す
 	Move();
 }
 
 void Title::Draw()
 {
-	//普通に描画するだけ
-	//DrawFormatString(GAME_WIDTH / 2 - 112*2.5f, GAME_HEIHGT / 2 , GetColor(255, 255, 255), "東方河本録");
 	// 描画する文字列のサイズを設定
 	SetFontSize(112);
+
+	//普通に描画するだけ
+	//DrawFormatString(GAME_WIDTH / 2 - 112*2.5f, GAME_HEIHGT / 2 , GetColor(255, 255, 255), "東方河本録");
+
 	//動かすなら
 	DrawFormatString(GAME_WIDTH / 2 - 64 - textSpeed[2] + o, GAME_HEIHGT / 2-200 - sin(rad) * 64 * 0.25f, GetColor(255, 255, 255), "東");
-	DrawFormatString(GAME_WIDTH / 2 - 64 - textSpeed[1] + o, GAME_HEIHGT / 2-200 - cos(rad) * 64 * 0.5f, GetColor(255, 255, 255), "方");
+	DrawFormatString(GAME_WIDTH / 2 - 64 - textSpeed[1] + o, GAME_HEIHGT / 2-200 - cos(rad) * 64 * 0.5f,  GetColor(255, 255, 255), "方");
 	DrawFormatString(GAME_WIDTH / 2 - 64 + textSpeed[0] + o, GAME_HEIHGT / 2-200 - sin(rad) * 64 * 0.05f, GetColor(255, 255, 255), "河");
-	DrawFormatString(GAME_WIDTH / 2 - 64 + textSpeed[1] + o, GAME_HEIHGT / 2-200 - cos(rad) * 64 * 0.5f, GetColor(255, 255, 255), "本");
+	DrawFormatString(GAME_WIDTH / 2 - 64 + textSpeed[1] + o, GAME_HEIHGT / 2-200 - cos(rad) * 64 * 0.5f,  GetColor(255, 255, 255), "本");
 	DrawFormatString(GAME_WIDTH / 2 - 64 + textSpeed[2] + o, GAME_HEIHGT / 2-200 - sin(rad) * 64 * 0.75f, GetColor(255, 255, 255), "録");
 
 	//梵天丸が存在感あり過ぎなので抑制しておく
@@ -68,6 +72,7 @@ void Title::Move()
 	cnt++;
 	//タイトルの移動処理
 	{
+		//if文の中とかマジックナンバーありありロジカル許して
 		if (deg >= 360)deg = 0;
 		rad = deg * 3.14 / 180;
 
@@ -98,8 +103,18 @@ void Title::Move()
 		if (w >= 450)w = 50;
 		// 描画する文字列のサイズを設定
 		SetFontSize(32);
-		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f, GetColor(255, 255, 255), "PRESS SPACE");
-		DrawBox(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.4f, GAME_WIDTH / 2 - 32 * 3.5f + 200, GAME_HEIHGT / 1.75f + w, GetColor(0, 0, 0), true);
-		DrawBox(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.4f-350, GAME_WIDTH / 2 - 32 * 3.5f + 200, GAME_HEIHGT / 1.75f + w-350, GetColor(0,0, 0), true);
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.25f, GetColor(255, 255, 255), "P");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.5f,  GetColor(255, 255, 255), " R");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.75f, GetColor(255, 255, 255), "  E");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2,     GetColor(255, 255, 255), "   S");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2.5f,  GetColor(255, 255, 255), "    S");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2.5f,  GetColor(255, 255, 255), "      S");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2,     GetColor(255, 255, 255), "       P");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.75,  GetColor(255, 255, 255), "        A");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.5f,  GetColor(255, 255, 255), "         C");
+		DrawFormatString(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.25f, GetColor(255, 255, 255), "          E");
+
+		DrawBox(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.4f      , GAME_WIDTH / 2 - 32 * 1.25f + 200, GAME_HEIHGT / 1.75f + w,     GetColor(0, 0, 0), true);
+		DrawBox(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.4f - 350, GAME_WIDTH / 2 - 32 * 3.5f  + 200, GAME_HEIHGT / 1.75f + w-350, GetColor(0, 0, 0), true);
 	}
 }
