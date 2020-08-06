5849a8fb05e7066621fc8f00c411bbc7ba6e419e
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-08-07 00:56:38 +0900

    [update]微修正

--- Source/Ui.h
+++ Source/Ui.h
@@ -18,7 +18,7 @@ private:
 	int Color;						//F
 
 public:
-	void Set_CastleDurability(int CastleDurability, int MAX_DURABILITY);
+	void Set_CastleDurability(int castledurability, int max_durabilty);
 	void Set_EnemyiesRest(int);
 	void PlayGuide();
 };
