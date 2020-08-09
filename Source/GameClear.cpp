5aa9b7ebe3a05b2331b987c9b5194d2d48c23fcf
kuroneko96 <kaityou5555@gmail.com>
2020-08-10 07:24:28 +0900

    [add]ゲームクリアシーンの追加

---
+++ Source/GameClear.cpp
@@ -0,0 +1,19 @@
+#include <DxLib.h>
+#include "GameClear.h"
+#include "Input.h"
+
+/*更新処理*/
+void GameClear::Update() {
+	if (Input::Instance()->GetPressCount(KEY_INPUT_SPACE) == 1) {
+		DrawString(0, 0, "メニューへ戻る処理", GetColor(255, 255, 255));
+	}
+}
+
+/*描画処理*/
+void GameClear::Draw() {
+	DrawBoxAA(50, 50, 874, 250, GetColor(255, 255, 255), TRUE);
+	DrawString(412-(13*7), 150+7, "Clearしたね！！おめでとう", GetColor(0, 0, 0));
+
+	DrawBoxAA(619, 263, 1019, 763, GetColor(255, 255, 255), TRUE);
+	DrawString(200, 568, "Press the space key to return menu", GetColor(255, 255, 255));
+}
---
+++ Source/GameClear.h
@@ -0,0 +1,12 @@
+#ifndef _GAMECLEAR_H
+#define _GAMECLEAR_H
+
+#include "BaseScene.h"
+
+class GameClear final :public BaseScene {
+public:
+	void Update();
+	void Draw();
+};
+
+#endif // !_GAMECLEAR_H
