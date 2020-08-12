8b27c05cfc93bae4bd1bd59b66d257becda9526c
kuroneko96 <kaityou5555@gmail.com>
2020-08-13 00:40:26 +0900

    [update]FPSの向上処理のための修正

--- Source/FPS.cpp
+++ Source/FPS.cpp
@@ -1,40 +1,35 @@
 #include <DxLib.h>
 #include "FPS.h"
 
-/*******************************************************************************
-参考サイト→https://dixq.net/s/2.html
-*******************************************************************************/
-
 FPS::FPS() {
-    fpsTime_i = 0;
-    nowFps = 0;
-    refreshTime = 0;
-    memset(fpsTime, 0, sizeof(fpsTime));
+	mStartTime = 0;
+	mCount = 0;
+	mFps = 0;
 }
 
 void FPS::Update() {
-    if (fpsTime_i == 0) {
-        fpsTime[0] = GetNowCount();
-    }
+	if (mCount == 0) { //1フレーム目なら時刻を記憶
+		mStartTime = GetNowCount();
+	}
+	if (mCount == N) { //60フレーム目なら平均を計算する
+		int t = GetNowCount();
+		mFps = 1000.f / ((t - mStartTime) / (float)N);
+		mCount = 0;
+		mStartTime = t;
 
-    if (fpsTime_i == 59) {
-        fpsTime[1] = GetNowCount();
-        nowFps = 1000.0f / ((fpsTime[1] - fpsTime[0]) / 60.0f);
-        fpsTime_i = 0;
-    }
-    else {
-        fpsTime_i++;
-    }
-
-    if (nowFps != 0)return;
-}
-
-void FPS::Draw()const {
-    DrawFormatString(0, 25, GetColor(255, 255, 255), "FPS:%.1f", nowFps);
+		return;
+	}
+	mCount++;
 }
 
 void FPS::Wait() {
-    refreshTime = GetNowCount();
+	int tookTime = GetNowCount() - mStartTime;	//かかった時間
+	int waitTime = mCount * 1000 / AVG_FPS - tookTime;	//待つべき時間
+	if (waitTime > 0) {
+		Sleep(waitTime);	//待機
+	}
+}
 
-    while (static_cast<float>(GetNowCount() - refreshTime) < 16.7f);
+void FPS::Draw() {
+	DrawFormatString(1024-75, 768-15, GetColor(255, 255, 255), "FPS:%.1f", mFps);
 }
