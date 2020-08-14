ef4a98e856032119790ba3f3d5ed611345a8628e
澤村遼 <ryo882514@gmail.com>
2020-08-14 16:56:55 +0900

    [update]文字の動きがFPS制御によって遅くなってたので              見た感じ違和感がないように修正

--- Source/Title.cpp
+++ Source/Title.cpp
@@ -1,29 +1,27 @@
 #include "Dxlib.h"
-#include "Title.h"
+#include "Define.h"
+#include "FontHandle.h"
 #include "Input.h"
 #include "math.h"
+#include "SE.h"
+#include "Title.h"
 
 /*コンストラクタ*/
 Title::Title(ISceneChanger* _sceneChanger) :BaseScene(_sceneChanger) {
-	//却下されれば消す
-	textSpeed[0] = 0;
-	textSpeed[1] = 1;
-	textSpeed[2] = 2;
+
+	textPosition = 875;
 	deg = 0;
 	rad = 0;
 	cnt = 0;
-	o = 1000;
-
-	//画像とか読み込みたいね
+	r = 0;
+	g = 0;
+	b = 0;
 }
 
 void Title::Update()
 {
 	if (Input::Instance()->GetPressCount(KEY_INPUT_SPACE) == 1)
 	{
-		//SE::Instance()->PlaySE(SE_00,No);
-		// 描画する文字列のサイズを設定
-		SetFontSize(16);
 		sceneChanger->SceneChange(eScene_MENU, false, false);
 	}
 
@@ -33,57 +31,60 @@ void Title::Update()
 void Title::Draw()
 {
 	//タイトル表示
-	DrawStringToHandle(GAME_WIDTH / 2 - 64 - textSpeed[2] + o, GAME_HEIHGT / 2 - 200 - sin(rad) * 64 * 0.25f, "東", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
-	DrawStringToHandle(GAME_WIDTH / 2 - 64 - textSpeed[1] + o, GAME_HEIHGT / 2 - 200 - cos(rad) * 64 * 0.5f , "方", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
-	DrawStringToHandle(GAME_WIDTH / 2 - 64 + textSpeed[0] + o, GAME_HEIHGT / 2 - 200 - sin(rad) * 64 * 0.05f, "河", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
-	DrawStringToHandle(GAME_WIDTH / 2 - 64 + textSpeed[1] + o, GAME_HEIHGT / 2 - 200 - cos(rad) * 64 * 0.5f , "本", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
-	DrawStringToHandle(GAME_WIDTH / 2 - 64 + textSpeed[2] + o, GAME_HEIHGT / 2 - 200 - sin(rad) * 64 * 0.75f, "録", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
+	DrawStringToHandle(GAME_WIDTH / 2 - 56 * 5 - sin(rad) * 0.5f + textPosition, GAME_HEIHGT / 2 - 200 - cos(rad) * 16, "東        ", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
+	DrawStringToHandle(GAME_WIDTH / 2 - 56 * 5 - sin(rad) * 0.5f + textPosition, GAME_HEIHGT / 2 - 200 - sin(rad) * 18, "  方      ", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
+	DrawStringToHandle(GAME_WIDTH / 2 - 56 * 5 - sin(rad) * 0.5f + textPosition, GAME_HEIHGT / 2 - 200 - cos(rad) * 10, "    河    ", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
+	DrawStringToHandle(GAME_WIDTH / 2 - 56 * 5 - sin(rad) * 0.5f + textPosition, GAME_HEIHGT / 2 - 200 - sin(rad) * 18, "      本  ", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
+	DrawStringToHandle(GAME_WIDTH / 2 - 56 * 5 - sin(rad) * 0.5f + textPosition, GAME_HEIHGT / 2 - 200 - cos(rad) * 25, "        録", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_112_3());
+
+	//プレススペースの表示処理
+	if(cnt == 250)
+	{
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.2f, "P          ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.4f, " R         ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.6f, "  E        ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.8f, "   S       ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 2,    "    S      ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 2,    "      S    ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.8f, "       P   ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.6f, "        A  ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.4f, "         C ", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		DrawStringToHandle(GAME_WIDTH / 2 - 16 * 5.5f, GAME_HEIHGT / 2 + 175 - sin(rad) * 1.2f, "          E", GetColor(r, b, g), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+	}
 
 	//画像表示今は四角表示
 	DrawBox(GAME_WIDTH -250, GAME_HEIHGT - 400, GAME_WIDTH, GAME_HEIHGT, GetColor(255, 255, 255), true);
 }
 
+//文字の動きの処理
 void Title::Move()
 {
-	cnt++;
-	//タイトルの移動処理
+	//角度計算
+	if (deg >= 360)deg = 0;
+	rad = deg * 3.14 / 180;
+
+	//画面中央まで横移動させる
+	if (cnt < 250)
 	{
-		//if文の中とかマジックナンバーありありロジカル許して
-		if (deg >= 360)deg = 0;
-		rad = deg * 3.14 / 180;
-
-		if (cnt < 112)
-		{
-			textSpeed[1] += 1;
-			textSpeed[2] += 2;
-		}
-		else
-		{
-			deg += 0.15f;
-		}
-
-		if (cnt < 500)
-		{
-			o -= 2;
-		}
-		else
-		{
-			deg += 0.15f;
-			o = deg * 3.14 / 720;
-			cnt = 500;
-		}
+		cnt++;
+		deg += 1.75f;
+		textPosition -= 3.5f;
 	}
-	//プレススペースの表示処理
+	else
 	{
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.25f, "P"          , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.5f,  " R"         , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.75f, "  E"        , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2,     "   S"       , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2.5f,  "    S"      , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2.5f,  "      S"    , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 2,     "       P"   , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.75,  "        A"  , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.5f,  "         C" , GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
-		DrawStringToHandle(GAME_WIDTH / 2 - 32 * 3.5f, GAME_HEIHGT / 1.5f - sin(rad) * 1.25f, "          E", GetColor(255, 255, 255), FontHandle::Instance()->Get_MS_GOTHIC_32_3());
+		deg += 1.25f;
+		//文字が浮かび上がるような感じにする
+		if (r < 255 && b < 255 && g < 255)
+		{
+			r++;
+			b++;
+			g++;
+		}
+		else
+		{
+			r = 255;
+			b = 255;
+			g = 255;
+		}
 	}
 }
