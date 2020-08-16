1d2a97e755a6adaf2cb6bf11c1087d97b2adbda9
kuroneko96 <kaityou5555@gmail.com>
2020-08-16 22:23:41 +0900

    [update]Update関数に引数を追加 [update]SetIsActive関数を追加

--- Source/BaseEnemy.h
+++ Source/BaseEnemy.h
@@ -5,6 +5,7 @@
 
 class Castle;
 class BasePlayer;
+class BulletManager;
 
 /*-------------------------------------------------------------
 オブジェクト名    BaseEnemy
@@ -42,20 +43,24 @@ protected:
 public:
 	BaseEnemy(){}
 	virtual ~BaseEnemy(){}
-
 	BaseEnemy(float _speed, float _power, int _durability, int _direction);
-	virtual void Update(Castle* _castle, BasePlayer* _player) {}	//更新処理
-	void Move();					//移動処理
+
+	virtual void Update(Castle* _castle, BasePlayer* _player,
+		BulletManager* _bulletManager) {}							//更新処理
+	virtual void Draw() {}											//描画処理
+
+	void Move();													//移動処理
 	void SearchCastle(float _ox, float _oy, float _ow, float _oh);	//城の範囲内かサーチ
 	void SearchPlayer(float _px, float _py, float _pw, float _ph,
 		BasePlayer* _player);										//プレイヤーが攻撃範囲内かサーチ
 	void JudgeActive();		//アクティブかを判断する
 	void AttackProc();		//攻撃中の時間処理
 
-	void Set_X(float _x) { x = _x; }						//x座標を設定する
-	void Set_Y(float _y) { y = _y; }						//y座標を設定する
-	void Set_Width(float _width) { width = _width; }		//widthを設定する
-	void Set_Height(float _height) { height = _height; }	//heightを設定する
+	void Set_X(float _x) { x = _x; }							//x座標を設定する
+	void Set_Y(float _y) { y = _y; }							//y座標を設定する
+	void Set_Width(float _width) { width = _width; }			//widthを設定する
+	void Set_Height(float _height) { height = _height; }		//heightを設定する
+	void SetIsAttack(bool _isAttack) { isAttack = _isAttack; }	
 
 	float Get_X() { return x; }				//x座標を取得する
 	float Get_Y() { return y; }				//y座標を取得する
@@ -65,11 +70,9 @@ public:
 	bool GetIsActive() { return isActive; }	//生存フラグを取得する
 	bool GetIsAttack() { return isAttack; }	//アタックフラグを取得する
 	
-	virtual bool ClisionHit(float mx, float my, float mw, float mh,
+	bool ClisionHit(float mx, float my, float mw, float mh,
 		float ox, float oy, float ow, float oh);
 	// m = my 自分   o = opnet 相手	//当たり判定処理
-
-	virtual void Draw() {}   //描画処理
 };
 
 #endif _BASEENEMY_HS
