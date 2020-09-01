6bd89c60bf59389e63e2981851cdc6df9876ea1f
dooooori <binamu1215@gmail.com>
2020-09-01 12:38:33 +0900

    [add]イベントのアクティブ状態のgetter追加

--- Source/BaseEvent.h
+++ Source/BaseEvent.h
@@ -5,6 +5,7 @@
 
 class BaseEvent {
 protected:
+	bool isActive = false;					//アクティブ状態
 
 public:
 	BaseEvent();					//コンストラクタ
@@ -14,6 +15,8 @@ public:
 	virtual void Update();			//更新
 
 	virtual void Draw();			//描画
+
+	bool GetIsActive() { return isActive; }	//アクティブ状態を取得する
 };
 
-#endif _BASEEVENT_H
+#endif _BASEEVENT_HS
