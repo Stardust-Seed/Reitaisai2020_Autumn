4f1f6a42a7dd5755f69be6035369d2b0007a2bfe
dooooori <binamu1215@gmail.com>
2020-09-02 15:18:43 +0900

    [update]objectを継承し忘れてたので更新

--- Source/BaseEvent.h
+++ Source/BaseEvent.h
@@ -3,20 +3,20 @@
 
 #include "Object.h"
 
-class BaseEvent {
+class BaseEvent : public virtual Object {
 protected:
-	bool isActive = false;					//アクティブ状態
+	bool isActive = false;			//アクティブ状態 雑に初期化
 
 public:
-	BaseEvent();					//コンストラクタ
+	BaseEvent() {}					//コンストラクタ
 	
-	virtual ~BaseEvent();			//デストラクタ
+	virtual ~BaseEvent(){}			//デストラクタ
 
-	virtual void Update();			//更新
+	virtual void Update() {}		//更新
 
-	virtual void Draw();			//描画
+	virtual void Draw() {}			//描画
 
 	bool GetIsActive() { return isActive; }	//アクティブ状態を取得する
 };
 
-#endif _BASEEVENT_HS
+#endif _BASEEVENT_H
