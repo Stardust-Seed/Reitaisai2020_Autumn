c93bca158784a8f910a08a757ca00381a9df895a
kuroneko96 <kaityou5555@gmail.com>
2020-09-03 19:20:06 +0900

    [add]ItemManagerの追加

---
+++ Source/ItemManager.cpp
@@ -0,0 +1,96 @@
+#include <DxLib.h>
+#include "ItemManager.h"
+#include "Item_P.h"
+#include "Item_S.h"
+
+ItemManager::ItemManager() {
+	p_Count = 0;
+	s_Count = 0;
+
+	for (int i = 0; i < MAX_ITEM_NUM; i++) {
+		items[i] = nullptr;
+	}
+}
+
+ItemManager::~ItemManager() {
+	for (int i = 0; i < MAX_ITEM_NUM; i++) {
+		delete items[i];
+	}
+}
+
+void ItemManager::Update(BasePlayer* _player) {
+	for (int i = 0; i < MAX_ITEM_NUM; i++) {
+
+		//items[i]が空でない場合
+		if (items[i] != nullptr) {
+
+			//items[i]が非アクティブの場合、items[i]を空にして次のループへ
+			if (items[i]->Get_IsActive() == false) {
+				delete items[i];
+				items[i] = nullptr;
+				continue;
+			}
+
+			//items[i]の更新処理
+			items[i]->Update(_player);
+
+			//items[i]がプレイヤーと当たってた場合
+			if (items[i]->Get_IsHit() == true) {
+
+				//items[i]のアイテム情報を取得し、対応した処理をおこなう
+				switch (items[i]->Get_ItemType())
+				{
+				//eItem::Powerの場合、p_Countを加算
+				case eItem::Power:
+					p_Count++;
+					break;
+				//eItem::Speedの場合、s_Countを加算
+				case eItem::Speed:
+					s_Count++;
+					break;
+				}
+			}
+		}
+	}
+}
+
+void ItemManager::Draw() {
+	for (int i = 0; i < MAX_ITEM_NUM; i++) {
+		if (items[i] != nullptr) {
+			items[i]->Draw();
+		}
+	}
+}
+
+void ItemManager::SpawnItem(float _ex,float _ey) {
+	bool isSpawn = false;	//生成判定フラグ
+
+	for (int i = 0; i < MAX_ITEM_NUM; i++) {
+		//items[i]が空の場合
+		if (items[i] == nullptr) {
+			int spawnPercent = GetRand(100);	//生成確率
+
+			//50未満の場合生成判定フラグをtrueにする
+			if (spawnPercent < ITEM_GENERATION_RATE) {
+				isSpawn = true;
+			}
+
+			//生成判定フラグがtrueの場合
+			if (isSpawn == true) {
+				int spawnType = GetRand(1);	//生成するアイテムのタイプを決める
+
+				//spawnTypeがeItem::Powerのとき
+				if (spawnType == static_cast<int>(eItem::Power)) {
+					items[i] = new Item_P(_ex, _ey, 16, 16, eItem::Power);
+				}
+
+				//spawnTypeがeItem::Powerのとき
+				if (spawnType == static_cast<int>(eItem::Speed)) {
+					items[i] = new Item_S(_ex, _ey, 16, 16, eItem::Speed);
+				}
+			}
+
+			break;
+		}
+	}
+}
---
+++ Source/ItemManager.h
@@ -0,0 +1,59 @@
+#ifndef _ITEMMANAGER_H
+#define _ITEMMANAGER_H
+
+#include "BaseItem.h"
+
+class BasePlayer;
+
+class ItemManager final {
+private:
+	static const int MAX_ITEM_NUM = 50;				//アイテムの画面上最大生成数
+	static const int ITEM_GENERATION_RATE = 100;	//アイテムの生成率
+
+	BaseItem* items[MAX_ITEM_NUM];	//アイテム
+
+	int p_Count;	//Pの取得数カウント
+	int s_Count;	//Sの取得数カウント
+public:
+	/// <summary>
+	/// ItemManagerのコンストラクタ
+	/// </summary>
+	ItemManager();
+
+	/// <summary>
+	/// ItemManagerのコンストラクタ
+	/// </summary>
+	~ItemManager();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	/// <param name="_player">プレイヤー情報</param>
+	void Update(BasePlayer* _player);
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+	/// <summary>
+	/// アイテムの生成処理
+	/// </summary>
+	/// <param name="_ex">エネミーのx座標</param>
+	/// <param name="_ey">エネミーのy座標</param>
+	void SpawnItem(float _ex, float _ey);
+
+	/// <summary>
+	/// p_Countのゲッター
+	/// </summary>
+	/// <returns>p_Count</returns>
+	int Get_P_Count() { return p_Count; }
+
+	/// <summary>
+	/// s_Countのゲッター
+	/// </summary>
+	/// <returns>s_Count</returns>
+	int Get_S_Count() { return s_Count; }
+};
+
+#endif // !_ITEMMANAGER_H
