4e817486cd5437d3068c08f77874dc246f68fdd9
澤村遼 <ryo882514@gmail.com>
2020-09-06 13:51:38 +0900

    [update]いらないとこ消したりコメント振ったり

--- Source/BaseCastle.cpp
+++ Source/BaseCastle.cpp
@@ -1,27 +1,16 @@
 #include "BaseCastle.h"
-#include "EnemyManager.h"
-#include "Define.h"
+
 const int BaseCastle::MAX_DURABILITY = 100;		//最大耐久力	
 
-
+//コンストラクタ
 BaseCastle::BaseCastle(int _durability){
-durability = _durability;
-isActive = true;
-isHit = false;
-width = 120;
-height = 120;
-x = GAME_WIDTH / 2 - width / 2;
-y = GAME_HEIHGT / 2 - height / 2;
 
-}
-
-void BaseCastle::Set_IsActive(bool _flg)
-{
-	isActive = _flg;
-}
-
-void BaseCastle::Set_Durability(int _durability) 
-{
 	durability = _durability;
-}
+	isActive = true;
+	isHit = false;
+	enemySpeed = 0;
+	enemyPower = 0;
+	enemyDurability = 0;
+	enemyDirection = 0;
 
+}
--- Source/BaseCastle.h
+++ Source/BaseCastle.h
@@ -22,11 +22,17 @@ protected:
 
 	bool isActive;		                    //拠点の生存フラグ
 
-public:
-	BaseCastle(int);                 //コンストラクタ
+	//敵に送る占領地点から出現する敵のパラメータ
+	float enemySpeed;                       //敵のスピード
+	float enemyPower;                       //敵のパワー
+	int enemyDurability;                    //敵の体力
+	int enemyDirection;                     //敵の方向
 
-	virtual void Update(EnemyManager*) {} 			//更新処理
-	virtual void Draw() {}			//描画処理
+public:
+	BaseCastle(int);                        //コンストラクタ
+
+	virtual void Update(EnemyManager*) {} 	//更新処理
+	virtual void Draw() {}		        	//描画処理
 
 	bool ClisionHit(float mx, float my, float mw, float mh,
 		float ox, float oy, float ow, float oh) 
@@ -36,17 +42,32 @@ public:
 	// m = my 自分   o = opnet 相手	//当たり判定処理
 
 	//ゲッター
-	bool GetIsActive() { return isActive; }
+	//拠点関連
+	int   Get_Durability() { return durability; }
 
-	int Get_Durability() { return durability; }
+	bool  GetIsActive()    { return isActive; }
 
 	float Get_X() { return x; }
 	float Get_Y() { return y; }
 	float Get_Width() { return width; }
 	float Get_Height() { return height; }
 
-	void Set_Durability(int);
-	void Set_IsActive(bool);
+	//敵パラメータ関連
+	float Get_EnemySpeed() { return enemySpeed; }
+	float Get_EnemyPower() { return enemyPower; }
+	int Get_EnemyDurability() { return enemyDurability; }
+	int Get_EnemyDirection() { return enemyDirection; }
+
+	//セッター
+	//拠点関連
+	void Set_Durability(int _durability) { durability = _durability; }
+	void Set_IsActive(bool _flg) { isActive = _flg; }
+
+	//敵パラメータ関連
+	void Set_EnemySpeed(float _speed) { enemySpeed = _speed; }
+	void Set_EnemyPower(float _power) { enemyPower = _power; }
+	void Set_EnemyDurability(int _durability) { enemyDurability = _durability; }
+	void Set_EnemyDirection(int _direction) { enemyDirection = _direction; }
 };
 
 #endif //_BASECASTLE_H_
