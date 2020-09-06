b75b66c4610a75bc46d00d932a7bbf012666b370
澤村遼 <ryo882514@gmail.com>
2020-09-06 15:37:13 +0900

    [update]ちょっと修正

--- Source/MainCastle.h
+++ Source/MainCastle.h
@@ -18,23 +18,12 @@ class EnemyManager;
 class MainCastle:public BaseCastle{
 
 private:
-	int durability;	                    //耐久力
-
-	bool hitFlg;                        //ヒットしたかのフラグ
-	bool isActive;	                    //拠点の生存フラグ
 
 public:
 	 MainCastle(int);
-	 //こっち使わない
-	 bool ClisionHit(float mx, float my, float mw, float mh,
-		 float ox, float oy, float ow, float oh) 
-	 {
-		 return isActive;
-	 }
-	 //m = my 自分   o = opnet 相手
 
-	 //こっち使う
-	 bool ClisionHit(float, float, float, float,int,int, bool,bool);
+	 bool ClisionHit(float ox, float oy, float ow, float oh,
+		  int pow, int num, bool attackFlg, bool activeFlg);
 
 	 void Update(EnemyManager*);               //更新処理
 	 void Draw();                              //描画処理
