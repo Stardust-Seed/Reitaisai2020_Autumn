529a256bedc09716343133036fd9b327647363f2
澤村遼 <ryo882514@gmail.com>
2020-09-09 02:18:53 +0900

    [fix]敵情報の削除

--- Source/BaseCastle.h
+++ Source/BaseCastle.h
@@ -19,14 +19,8 @@ protected:
 	static const int MAX_DURABILITY;		//最大耐久力			
 
 	int durability;		                    //拠点の体力
-
+	int direction;
 	bool isActive;		                    //拠点の生存フラグ
-
-	//敵に送る占領地点から出現する敵のパラメータ
-	float enemySpeed;                       //敵のスピード
-	float enemyPower;                       //敵のパワー
-	int enemyDurability;                    //敵の体力
-	int enemyDirection;                     //敵の方向
 
 public:
 	BaseCastle(int);                        //コンストラクタ
@@ -34,12 +28,16 @@ public:
 	virtual void Update(EnemyManager*) {} 	//更新処理
 	virtual void Draw() {}		        	//描画処理
 
+	//こっちは使わない
 	bool ClisionHit(float mx, float my, float mw, float mh,
-		float ox, float oy, float ow, float oh) 
-	{
-		return isActive;
+		float ox, float oy, float ow, float oh) {
+		return false;
 	}
 	// m = my 自分   o = opnet 相手	//当たり判定処理
+
+	//こっち使う
+	bool ClisionHit(float ox, float oy, float ow, float oh,
+		int pow, int num, bool attackFlg, bool activeFlg);
 
 	//ゲッター
 	//拠点関連
@@ -52,22 +50,7 @@ public:
 	float Get_Width() { return width; }
 	float Get_Height() { return height; }
 
-	//敵パラメータ関連
-	float Get_EnemySpeed() { return enemySpeed; }
-	float Get_EnemyPower() { return enemyPower; }
-	int Get_EnemyDurability() { return enemyDurability; }
-	int Get_EnemyDirection() { return enemyDirection; }
-
-	//セッター
-	//拠点関連
-	void Set_Durability(int _durability) { durability = _durability; }
-	void Set_IsActive(bool _flg) { isActive = _flg; }
-
-	//敵パラメータ関連
-	void Set_EnemySpeed(float _speed) { enemySpeed = _speed; }
-	void Set_EnemyPower(float _power) { enemyPower = _power; }
-	void Set_EnemyDurability(int _durability) { enemyDurability = _durability; }
-	void Set_EnemyDirection(int _direction) { enemyDirection = _direction; }
+	int Get_CastleDirection() { return direction; }
 };
 
 #endif //_BASECASTLE_H_
