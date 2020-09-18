4970d339e868a3b325c297068db220329cfecfb9
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-09-19 00:10:28 +0900

    [update]表示する場所がなんかのバグでおかしかったので修正

--- Source/Ui.h
+++ Source/Ui.h
@@ -22,31 +22,31 @@ private:
 	static const int HPGAUGE_X1 = 50;			//バーのｘ終点
 	static const int HPGAUGE_Y1 = 10;	
 	static const int MAX_DURABILTY = 100;		//メイン拠点最大体力
-	static const int MAX_SABDURABILTY = 50;	//サブ拠点最大体力
+	static const int MAX_SABDURABILTY = 10;	//サブ拠点最大体力
 
-	static const int PMOJI_X = 1150;			//文字のｘ
-	static const int PMOJI_Y = 760;
-	static const int PBAR_X = 1200;				//枠のｘ始点
-	static const int PBAR_Y = 760;
-	static const int PBAR_X1 = 1200;			//枠のｘ終点
-	static const int PBAR_Y1 = 760;
-	static const int PGAUGE_X = 1200;			//バーのｘ始点
-	static const int PGAUGE_Y = 760;
-	static const int PGAUGE_X1 = 1200;			//バーのｘ終点
-	static const int PGAUGE_Y1 = 760;
+	static const int PMOJI_X = 1550;			//文字のｘ
+	static const int PMOJI_Y = 960;
+	static const int PBAR_X = 1600;				//枠のｘ始点
+	static const int PBAR_Y = 960;
+	static const int PBAR_X1 = 1600;			//枠のｘ終点
+	static const int PBAR_Y1 = 960;
+	static const int PGAUGE_X = 1600;			//バーのｘ始点
+	static const int PGAUGE_Y = 960;
+	static const int PGAUGE_X1 = 1600;			//バーのｘ終点
+	static const int PGAUGE_Y1 = 960;
 
-	static const int SMOJI_X = 1150;			//文字のｘ
-	static const int SMOJI_Y = 800;
-	static const int SBAR_X = 1200;				//枠のｘ始点
-	static const int SBAR_Y = 800;
-	static const int SBAR_X1 = 1200;			//枠のｘ終点
-	static const int SBAR_Y1 = 800;
-	static const int SGAUGE_X = 1200;			//バーのｘ始点
-	static const int SGAUGE_Y = 800;
-	static const int SGAUGE_X1 = 1200;			//バーのｘ終点
-	static const int SGAUGE_Y1 = 800;
+	static const int SMOJI_X = 1550;			//文字のｘ
+	static const int SMOJI_Y = 1000;
+	static const int SBAR_X = 1600;				//枠のｘ始点
+	static const int SBAR_Y = 1000;
+	static const int SBAR_X1 = 1600;			//枠のｘ終点
+	static const int SBAR_Y1 = 1000;
+	static const int SGAUGE_X = 1600;			//バーのｘ始点
+	static const int SGAUGE_Y = 1000;
+	static const int SGAUGE_X1 = 1600;			//バーのｘ終点
+	static const int SGAUGE_Y1 = 1000;
 
-	const float MAX_BUFF = 10.0;				//バフゲージの最大数
+	const int MAX_BUFF = 15;				//バフゲージの最大数
 
 	int percent;					//パーセント
 	int CastleDurability;			//拠点の体力
@@ -54,7 +54,6 @@ private:
 	int sBuffPoint;					//sアイテムのゲージ上昇用
 	int pBuffLevel;					//pアイテム獲得時のバフレベル
 	int sBuffLevel;					//sアイテム獲得時のバフレベル
-	int Color;						//色
 
 public:
 	void Get_CastleDurability();		//メイン拠点の体力UI
