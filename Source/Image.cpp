ea525097bc5cced326b2fb4dd083a659999e6866
kuroneko96 <kaityou5555@gmail.com>
2020-09-20 00:29:09 +0900

    [update]eImageTypeをenumからenum classへ [update]画像データをゲームへ反映準備 [update]コメントを追加

--- Source/Image.cpp
+++ Source/Image.cpp
@@ -2,8 +2,21 @@
 #include "Image.h"
 
 Image::Image() {
-	MyLoadGraph("./res/Image/Test.png");
-	MyLoadDivGraph("./res/Image/Test.png", 4, 2, 2, 512, 288);
+	//‰æ‘œ‚Ì“Ç‚İ‚İ
+	MyLoadGraph("./res/Image/“Œ•û–h‰q‘à.png");
+	MyLoadGraph("./res/Image/darkness.png");
+	MyLoadGraph("./res/Image/sakuya_tatie01.png");
+	MyLoadGraph("./res/Image/sakuya_tatie02.png");
+	MyLoadGraph("./res/Image/sakuya_tatie03.png");
+	MyLoadGraph("./res/Image/Fran_tatie01.png");
+	MyLoadGraph("./res/Image/Fran_tatie02.png");
+	MyLoadGraph("./res/Image/Fran_tatie03.png");
+	MyLoadGraph("./res/Image/sakuya01.png");
+	MyLoadGraph("./res/Image/sakuya02.png");
+	MyLoadGraph("./res/Image/Fran01.png");
+	MyLoadGraph("./res/Image/Fran02.png");
+	MyLoadGraph("./res/Image/enemy01.png");
+	MyLoadGraph("./res/Image/enemy02.png");
 }
 
 void Image::Release() {
@@ -14,19 +27,36 @@ void Image::Release() {
 }
 
 int Image::GetGraph(eImageType _imageType, int _num) {
-
-	int identIndex = 0;
-
-	switch (_imageType)
-	{
-	case eImageType_Test:
-		identIndex = 0;
-		return images[identIndex];
-		break;
-	case eImageType_Test2:
-		identIndex = 1;
-		return images[identIndex + _num];
-		break;
+	//o—Í‚·‚é‰æ‘œ‚ğ‘I‘ğ
+	switch (_imageType) {
+	case eImageType::Title_Logo:
+		return images[static_cast<int>(eImageType::Title_Logo)];
+	case eImageType::Event_Darkness:
+		return images[static_cast<int>(eImageType::Event_Darkness)];
+	case eImageType::Spicture_Sakuya01:
+		return images[static_cast<int>(eImageType::Spicture_Sakuya01)];
+	case eImageType::Spicture_Sakuya02:
+		return images[static_cast<int>(eImageType::Spicture_Sakuya02)];
+	case eImageType::Spicture_Sakuya03:
+		return images[static_cast<int>(eImageType::Spicture_Sakuya03)];
+	case eImageType::Spicture_Fran01:
+		return images[static_cast<int>(eImageType::Spicture_Fran01)];
+	case eImageType::Spicture_Fran02:
+		return images[static_cast<int>(eImageType::Spicture_Fran02)];
+	case eImageType::Spicture_Fran03:
+		return images[static_cast<int>(eImageType::Spicture_Fran03)];
+	case eImageType::Gpicture_Sakuya01:
+		return images[static_cast<int>(eImageType::Gpicture_Sakuya01)];
+	case eImageType::Gpicture_Sakuya02:
+		return images[static_cast<int>(eImageType::Gpicture_Sakuya02)];
+	case eImageType::Gpicture_Fran01:
+		return images[static_cast<int>(eImageType::Gpicture_Fran01)];
+	case eImageType::Gpicture_Fran02:
+		return images[static_cast<int>(eImageType::Gpicture_Fran02)];
+	case eImageType::Gpicture_Enemy01:
+		return images[static_cast<int>(eImageType::Gpicture_Enemy01)];
+	case eImageType::Gpicture_Enemy02:
+		return images[static_cast<int>(eImageType::Gpicture_Enemy02)];
 	default:
 		exit(EXIT_FAILURE);
 		break;
--- Source/Image.h
+++ Source/Image.h
@@ -4,23 +4,78 @@
 #include <vector>
 #include "Singleton.h"
 
-enum eImageType {
-	eImageType_Test,
-	eImageType_Test2
+/// <summary>
+/// 画像のタイプ
+/// </summary>
+enum class eImageType {
+	Title_Logo,			//タイトルロゴ
+	Event_Darkness,		//ダークネスイベントの画像
+	Spicture_Sakuya01,	//立ち絵 咲夜(通常)
+	Spicture_Sakuya02,	//立ち絵 咲夜(笑顔)
+	Spicture_Sakuya03,	//立ち絵 咲夜(焦り)
+	Spicture_Fran01,	//立ち絵 フラン(通常)
+	Spicture_Fran02,	//立ち絵 フラン(笑顔)
+	Spicture_Fran03,	//立ち絵 フラン(焦り？)
+	Gpicture_Sakuya01,	//ゲームの素材 昨夜01
+	Gpicture_Sakuya02,	//ゲームの素材 昨夜02
+	Gpicture_Fran01,	//ゲームの素材 フラン01
+	Gpicture_Fran02,	//ゲームの素材 フラン02
+	Gpicture_Enemy01,	//ゲームの素材 敵01
+	Gpicture_Enemy02	//ゲームの素材 敵02
 };
 
+/// <summary>
+/// 画像クラス
+/// </summary>
 class Image final :public Singleton<Image> {
 private:
-	std::vector<int> images;
+	std::vector<int> images;	//画像データ
 
+	/// <summary>
+	/// 画像を読み込む
+	/// </summary>
+	/// <param name="_filePath">読み込む画像のファイルパス</param>
 	void MyLoadGraph(const char* _filePath);
+
+	/// <summary>
+	/// 画像を分割して読み込む
+	/// </summary>
+	/// <param name="_filePath">読み込む画像のファイルパス</param>
+	/// <param name="_allNum">分割する総数</param>
+	/// <param name="_xNum">横方向の分割数</param>
+	/// <param name="_yNum">縦方向の分割数</param>
+	/// <param name="_xSize">分割した画像の横幅</param>
+	/// <param name="_ySize">分割した画像の縦幅</param>
 	void MyLoadDivGraph(const char* _filePath, int _allNum, int _xNum, int _yNum,
 		int _xSize, int _ySize);
 public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <returns></returns>
 	Image();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	~Image() = default;
+
+	/// <summary>
+	/// 画像をロードする
+	/// </summary>
 	void Load(){}
+
+	/// <summary>
+	/// 画像を取得する
+	/// </summary>
+	/// <param name="_imageType">画像の種類</param>
+	/// <param name="_num">分割した画像の番号</param>
+	/// <returns>選択された画像</returns>
 	int GetGraph(eImageType _imageType, int _num = 0);
+
+	/// <summary>
+	/// 画像データの開放
+	/// </summary>
 	void Release();
 };
 
