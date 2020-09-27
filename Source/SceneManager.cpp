100efdaedc6b668c8c7ad27c3135a66f65ea34e5
kuroneko96 <kaityou5555@gmail.com>
2020-09-27 23:22:22 +0900

    [update]パラメータの追加

--- Source/SceneManager.cpp
+++ Source/SceneManager.cpp
@@ -1,16 +1,18 @@
 #include "SceneManager.h"
+#include "Charaselect.h"
 #include "GameClear.h"
 #include "GameOver.h"
 #include "GameScene.h"
+#include "LevelSelect.h"
 #include "Menu.h"
 #include "Option.h"
-#include "Charaselect.h"
+#include "Parameter.h"
 #include "Pausemenu.h"
 #include "Title.h"
 
 /*コンストラクタ*/
 SceneManager::SceneManager() {
-	scenes.push(std::make_shared<GameScene>(this));
+	scenes.push(std::make_shared<GameScene>(this, &parameter));
 }
 
 /*更新処理*/
@@ -24,7 +26,8 @@ void SceneManager::Draw() {
 }
 
 /*シーン切り替え処理*/
-void SceneManager::SceneChange(eScene _nextScene, const bool _isStack, const bool _isBack) {
+void SceneManager::SceneChange(eScene _nextScene, Parameter* _parameter,
+	const bool _isStack, const bool _isBack) {
 	//一つ前のシーンにする処理
 	if (_isBack) {
 		scenes.pop();
@@ -41,30 +44,31 @@ void SceneManager::SceneChange(eScene _nextScene, const bool _isStack, const boo
 
 	switch (_nextScene) {
 	case eScene_TITLE:
-		scenes.push(std::make_shared<Title>(this));
+		scenes.push(std::make_shared<Title>(this, _parameter));
 		break;
 	case eScene_MENU:
-		scenes.push(std::make_shared<Menu>(this));
+		scenes.push(std::make_shared<Menu>(this, _parameter));
 		break;
 	case eScene_GAME:
-		scenes.push(std::make_shared<GameScene>(this));
+		scenes.push(std::make_shared<GameScene>(this, _parameter));
 		break;
 	case eScene_CLAER:
-		scenes.push(std::make_shared<GameClear>(this));
+		scenes.push(std::make_shared<GameClear>(this, _parameter));
 		break;
 	case eScene_GAMEOVER:
-		scenes.push(std::make_shared<GameOver>(this));
+		scenes.push(std::make_shared<GameOver>(this, _parameter));
 		break;
 	case eScene_OPTION:
-		scenes.push(std::make_shared<Option>(this));
+		scenes.push(std::make_shared<Option>(this, _parameter));
 		break;
 	case eScene_PAUSEMENU:
-		scenes.push(std::make_shared<Pausemenu>(this));
+		scenes.push(std::make_shared<Pausemenu>(this, _parameter));
 		break;
 	case eScene_CHARASELECT:
-		scenes.push(std::make_shared<Charaselect>(this));
+		scenes.push(std::make_shared<Charaselect>(this, _parameter));
 		break;
 	case eScene_LEVELSELECT:
+		scenes.push(std::make_shared<LevelSelect>(this, _parameter));
 		break;
 	}
 }
