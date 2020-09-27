b2d35896bf7097185d14928886d19503ba645360
Flamberg3983 <masaki1217jp@gmail.com>
2020-09-27 23:52:47 +0900

    [update]シーン移動処理の変更

--- Source/Charaselect.cpp
+++ Source/Charaselect.cpp
@@ -35,15 +35,17 @@ void Charaselect::Update()
 		switch (charaSelect){
 		case select_SAKUYA:   //咲夜を選択
 			//確か次難易度選択画面だったっけ？
-			//sceneChanger->SceneChange(, false, false);
+			//sceneChanger->SceneChange(難易度画面, parameter, true, false);
 			break;
 		case select_FRAN:
 			//確か次難易度選択画面だったっけ？
-			//sceneChanger->SceneChange(, false, false);
+			//sceneChanger->SceneChange(難易度画面, false, false);
+			//sceneChanger->SceneChange(eScene_MENU, parameter, true, false);
 			break;
 		case select_BACK:
 			//前のシーン(画面)に戻る
 			sceneChanger->SceneChange(eScene_MENU, false, false);
+			//sceneChanger->SceneChange(eScene_MENU, parameter, true, false); //新
 			break;
 		}
 	}
@@ -55,9 +57,18 @@ void Charaselect::Draw()
 {
 	DrawFormatString(200, 200, GetColor(255, 255, 255),"選択項目:%d",charaSelect );
 
+	/***テスト用***/
+	static int select_Sakuya = LoadGraph("res/Image/select_Sakuya.png");
+	static int select_Fran = LoadGraph("res/Image/select_Fran.png");
+
+	DrawGraph(575, 200, select_Sakuya, true);
+	DrawGraph(1125, 200, select_Fran, true);
+
 	DrawBox(575, 200, 850, 600, GetColor(255, 255, 255), false);
+	//275*400
 
 	DrawBox(1125, 200, 1400, 600, GetColor(255, 255, 255), false);
+	//275*400
 
 	//キャラクター項目の描画
 	for (int i = 0; i < 2; i++)
