340cb7170a2881cc6ca49808ac56fb4a2c7b35dd
Flamberg3983 <masaki1217jp@gmail.com>
2020-09-29 00:53:35 +0900

    [update]キャラ選択画面をいい感じに

--- Source/CharaSelect.h
+++ Source/CharaSelect.h
@@ -1,7 +1,11 @@
 #ifndef _CHARASELECT_H
 #define _CHARASELECT_H
 
+#include <DxLib.h>
 #include"BaseScene.h"
+#include "FontHandle.h"
+#include"Image.h"
+#include"SE.h"
 
 //キャラクタータイプ
 enum SelectCharacter {
@@ -11,32 +15,35 @@ enum SelectCharacter {
 	select_NUM         //項目数
 };
 
-//選択項目の描画の為の構造体
-struct SelectMenu {
-	int x, y;    //表示座標
-	char isMenu[128]; //項目名
+//選択項目
+struct SelectMenu{
+	float x, y;   //描画位置
+	float rx, ry; //円の大きさ
 };
 
 class CharaSelect :public BaseScene {
 private:
 
-	int charaSelect;         //選択したキャラタイプ
-	//SelectCharacter charaType;	//選択したキャラタイプ
+	int charaSelect;                    //選択したキャラタイプ
 
-	SelectMenu selectMenu[2] = {
-		{660,700,"十六夜 咲夜"},
-		{1200,700,"フランドール・S"},
+	int color;    //色
+
+	SelectMenu selectMenu[3] = {
+		{685.0f,725.0f,160.0f,80.0f},    //咲夜
+		{1250.0f,725.0f,190.0f,80.0f},   //フラン
+		{970.0f,920.0f,180.0f,80.0f},    //戻る
 	};
 
 public:
 	CharaSelect(ISceneChanger* _sceneChanger, Parameter* _parameter);
 	void Update();
 	void Draw();
+	void Draw_Charaname();
+	void Draw_Charagraph();
+	void Draw_waku();
 
 	int Get_selectChara() { return charaSelect; }
 
 };
-
-
 
 #endif //CHARAselect_H_
