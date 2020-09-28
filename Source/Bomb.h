11c9bdea51f298256571566ff9471cd7e6a07e98
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-09-29 01:02:31 +0900

    [update]画像を追加、いらんものを消したと思う

--- Source/Bomb.h
+++ Source/Bomb.h
@@ -7,9 +7,7 @@ class Bomb : public virtual BaseBomb {
 private:
 	const int BOMBDAMAGE = 10;
 
-	int gBomb;
-	int m_Image;	        // 画像ハンドル
-	int GHandle[6];         // 格納用画像ハンドル
+	int gBomb[1];
 	int act_frameIndex[12] = { 0, 1, 2, 2, 3, 4, 5, 4, 3, 2, 1, 0};    // 表示する番号
 	int m_frameIndex = 0;
 public:
--- Source/FakeBomb.h
+++ Source/FakeBomb.h
@@ -7,12 +7,16 @@ class FakeBomb : public virtual BaseBomb
 {
 private:
 	int fBomb;
-	int fAction;
+	int GHandle[3];      // 格納用画像ハンドル
+	int act_frameIndex[8] = { 0, 1, 1, 1, 2, 2, 2 };   // 表示する番号
+	int m_frameIndex = 0;
+
 	bool isFakeAction;
 public:
 	FakeBomb() = default;
 	FakeBomb(int _power, int _speed, eBombType _bombType);
 	void FakeMotion();
+	void Animation();
 	void Update();
 	void Draw();
 };
