ace68e5f4fff08b000d54353698fe55ef391aed4
澤村遼 <ryo882514@gmail.com>
2020-09-30 17:18:12 +0900

    [update]UI関連の変更

--- Source/GameOver.h
+++ Source/GameOver.h
@@ -8,23 +8,15 @@
 #include "SE.h"
 #include "Define.h"
 #include "FontHandle.h"
-
-enum Continuity                 //選択させるのに使います
-{
-	//後々、項目が増えてもいいようにmin,maxの項目を設定しておく
-	Continuity_Min,             //最低値
-	Continuity_Yes,             //コンティニューする
-    Continuity_No,              //コンティニューしない
-	Continuity_Max              //最大値
-};
+#include "Cursor.h"
 
 class GameOver :public virtual BaseScene{
 
 private:
-	const static int GAME_X = GAME_WIDTH / 7.5f - 45;    //コンティニューすると表示するテキストのY座標
-	const static int MENU_X	= GAME_WIDTH / 2.5f - 30;    //コンティニューしないと表示するテキストのY座標
+	const static int GAME_X = GAME_WIDTH / 4.5f - 13;    //コンティニューすると表示するテキストのY座標
+	const static int MENU_X	= GAME_WIDTH / 2 - 20 ;    //コンティニューしないと表示するテキストのY座標
 
-	int nowCursor;              //現在選択中のカーソル
+	Cursor nowCursor;           //現在選択中のカーソル
 	int x;                      //カーソル表示用のX座標
 	int alpha;                  //透明度
 	int charaType;              //キャラタイプ
