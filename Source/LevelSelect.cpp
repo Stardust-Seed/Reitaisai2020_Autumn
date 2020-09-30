950e376cea0691d4973cdc543c8ac73a917e7568
kuroneko96 <kaityou5555@gmail.com>
2020-10-01 00:27:58 +0900

    [update]共通処理を関数化しました [update]DrawUIBoxの仕様変更に伴る修正

--- Source/LevelSelect.cpp
+++ Source/LevelSelect.cpp
@@ -3,6 +3,7 @@
 
 #include "Define.h"
 #include "FontHandle.h"
+#include "Image.h"
 #include "Input.h"
 #include "SE.h"
 
@@ -12,22 +13,16 @@ LevelSelect::LevelSelect(ISceneChanger* _sceneChanger, Parameter* _parameter)
 	selectLevel = eLevelType::Easy;
 
 	//Easyの初期座標
-	easyX1 = (GAME_WIDTH / 2) - 300;
-	easyX2 = (GAME_WIDTH / 2) + 300;
-	easyY1 = (GAME_HEIHGT / 2) - 400;
-	easyY2 = (GAME_HEIHGT / 2) - 200;
+	easyX = (GAME_WIDTH / 2);
+	easyY = (GAME_HEIHGT / 2) - 300;
 
 	//Normalの初期座標
-	normalX1 = (GAME_WIDTH / 2) - 300;
-	normalX2 = (GAME_WIDTH / 2) + 300;
-	normalY1 = (GAME_HEIHGT / 2) - 100;
-	normalY2 = (GAME_HEIHGT / 2) + 100;
+	normalX = (GAME_WIDTH / 2);
+	normalY = (GAME_HEIHGT / 2);
 
 	//Hardの初期座標
-	hardX1 = (GAME_WIDTH / 2) - 300;
-	hardX2 = (GAME_WIDTH / 2) + 300;
-	hardY1 = (GAME_HEIHGT / 2) + 200;
-	hardY2 = (GAME_HEIHGT / 2) + 400;
+	hardX = (GAME_WIDTH / 2);
+	hardY = (GAME_HEIHGT / 2) + 300;
 
 	//アニメーションカウントの初期化
 	animationCnt = 0;
@@ -43,27 +38,7 @@ void LevelSelect::Update() {
 	------------------------------------------*/
 	if (Input::Instance()->GetPressCount(KEY_INPUT_DOWN) % 16 == 1 &&
 		Input::Instance()->GetPressCount(KEY_INPUT_UP) == 0) {
-		/*--------------------------------------
-		現在選択中のレベルの下の項目に切り替える
-		※Hardの場合一番上のEasyに移動する
-		--------------------------------------*/
-		switch (selectLevel) {
-		case eLevelType::Easy:
-			selectLevel = eLevelType::Normal;
-			break;
-		case eLevelType::Normal:
-			selectLevel = eLevelType::Hard;
-			break;
-		case eLevelType::Hard:
-			selectLevel = eLevelType::Easy;
-			break;
-		}
-
-		//フラグを切り替える
-		isChange = true;
-
-		//SEを鳴らす
-		SE::Instance()->PlaySE(SE_cursor);
+		ChangeLevel(CURSOR_DOWN);
 	}
 
 	/*------------------------------------------
@@ -72,27 +47,7 @@ void LevelSelect::Update() {
 	------------------------------------------*/
 	if (Input::Instance()->GetPressCount(KEY_INPUT_UP) % 16 == 1 &&
 		Input::Instance()->GetPressCount(KEY_INPUT_DOWN) == 0) {
-		/*--------------------------------------
-		現在選択中のレベルの上の項目に切り替える
-		※Easyの場合一番下のHardに移動する
-		--------------------------------------*/
-		switch (selectLevel) {
-		case eLevelType::Easy:
-			selectLevel = eLevelType::Hard;
-			break;
-		case eLevelType::Normal:
-			selectLevel = eLevelType::Easy;
-			break;
-		case eLevelType::Hard:
-			selectLevel = eLevelType::Normal;
-			break;
-		}
-
-		//フラグを切り替える
-		isChange = true;
-
-		//SEを鳴らす
-		SE::Instance()->PlaySE(SE_cursor);
+		ChangeLevel(CURSOR_UP);
 	}
 
 	//zキーが押されたとき
@@ -126,40 +81,79 @@ void LevelSelect::Draw() {
 	UIの描画
 	-----------------------------------------------------------------------*/
 	//Easy
-	DrawUIBox(easyX1, easyY1, easyX2, easyY2, GetColor(0, 255, 0),
-		"Easy", eLevelType::Easy);
+	DrawUIBox(easyX, easyY, EASY, eLevelType::Easy, Cursor::Cursor_2);
 
 	//Normal
-	DrawUIBox(normalX1, normalY1, normalX2, normalY2, GetColor(0, 0, 255),
-		"Normal", eLevelType::Normal);
+	DrawUIBox(normalX, normalY, NORMAL, eLevelType::Normal, Cursor::Cursor_3);
 
 	//Hard
-	DrawUIBox(hardX1, hardY1, hardX2, hardY2, GetColor(255, 0, 0),
-		"Hard", eLevelType::Hard);
+	DrawUIBox(hardX, hardY, HARD, eLevelType::Hard, Cursor::Cursor_1);
 }
 
-void LevelSelect::ChangeLevel() {
+void LevelSelect::ChangeLevel(int _changeMode) {
+	//フラグを切り替える
+	isChange = true;
 
+	//SEを鳴らす
+	SE::Instance()->PlaySE(SE_cursor);
+
+	//選択されたレベルを返す
+	switch (selectLevel) {
+	case eLevelType::Easy:
+		//カーソルUPのとき
+		if (_changeMode == CURSOR_UP) {
+			selectLevel = eLevelType::Hard;
+		}
+		//カーソルDOWNのとき
+		else if (_changeMode == CURSOR_DOWN) {
+			selectLevel = eLevelType::Normal;
+		}
+		break;
+	case eLevelType::Normal:
+		//カーソルUPのとき
+		if (_changeMode == CURSOR_UP) {
+			selectLevel = eLevelType::Easy;
+		}
+		//カーソルDOWNのとき
+		else if (_changeMode == CURSOR_DOWN) {
+			selectLevel = eLevelType::Hard;
+		}
+		break;
+	case eLevelType::Hard:
+		//カーソルUPのとき
+		if (_changeMode == CURSOR_UP) {
+			selectLevel = eLevelType::Normal;
+		}
+		//カーソルDOWNのとき
+		else if (_changeMode == CURSOR_DOWN) {
+			selectLevel = eLevelType::Easy;
+		}
+		break;
+	}
 }
 
 //UIの描画処理
-void LevelSelect::DrawUIBox(float _x1, float _y1, float _x2, float _y2, unsigned int _color,
-	const char* _text, eLevelType _levelType) {
+void LevelSelect::DrawUIBox(float _x, float _y, string _text,
+	eLevelType _levelType, Cursor _cursor) {
+
+	int textSize = _text.size();
 
 	//選択されていないレベルだったら灰色にする
 	if (selectLevel != _levelType) {
-		_color = GetColor(125, 125, 125);
+		_cursor = Cursor::Cursor_0;
 	}
 
 	//ブレンドモードをアルファブレンドにする
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 125);
 
 	//四角形の描画
-	DrawBoxAA(_x1, _y1, _x2, _y2, _color, TRUE);
+	DrawRotaGraph3F(_x, _y, 650 / 2, 250 / 2, 1.0, 1.0, (0 * PAI) / 180,
+		Image::Instance()->GetGraph(eImageType::UI_CursorFrame, static_cast<int>(_cursor)),
+		FALSE);
 
 	//文字の描画
-	DrawStringToHandle(_x1, _y1, _text, GetColor(255, 255, 255),
-		FontHandle::Instance()->Get_natumemozi_100_3());
+	DrawStringToHandle(_x - ((100 / 2) * (textSize / 2)), _y - (100 / 2),
+		_text.c_str(), GetColor(255, 255, 255), FontHandle::Instance()->Get_natumemozi_100_3());
 
 	//ブレンドモードをノーブレンドにする
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
