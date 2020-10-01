4faf3c817d3bdb7c67bc89353cb52f86ffb711f7
澤村遼 <ryo882514@gmail.com>
2020-10-01 16:53:55 +0900

    [update]フォントの追加、テスト用のフォント削除

--- Source/FontHandle.cpp
+++ Source/FontHandle.cpp
@@ -5,19 +5,20 @@ FontHandle::FontHandle() {
 	//コンストラクタにInitの処理内容を直接入れるとなんか実行されなく、
 	//Initに処理を入れてコンストラクタに入れるとなんか実行されます
 	Init();
-	snoutCrayon = "./res/Font/crayon_1-1.ttf";             //読み込むフォントファイルのパス
+
 	natumemozi = "./res/Font/natumemozi.ttf";              //読み込むフォントファイルのパス
-	weakForce = "./res/Font/851CHIKARA-YOWAKU_002.ttf";   //読み込むフォントファイルのパス
-	AddFontResourceEx(snoutCrayon, FR_PRIVATE, NULL);      //フォントを読み込む
+	weakForce = "./res/Font/851CHIKARA-YOWAKU_002.ttf";    //読み込むフォントファイルのパス
+
 	AddFontResourceEx(natumemozi, FR_PRIVATE, NULL);       //フォントを読み込む
-	AddFontResourceEx(weakForce, FR_PRIVATE, NULL);      //フォントを読み込む
+	AddFontResourceEx(weakForce, FR_PRIVATE, NULL);        //フォントを読み込む
+
 }
 
 //デストラクタ
 FontHandle::~FontHandle() {
-	RemoveFontResource(snoutCrayon);                      //読み込んだフォントを削除
-	RemoveFontResource(natumemozi);                       //読み込んだフォントを削除
-	RemoveFontResource(weakForce);                        //読み込んだフォントを削除
+	RemoveFontResource(natumemozi);                        //読み込んだフォントを削除
+	RemoveFontResource(weakForce);                         //読み込んだフォントを削除
+
 	//作成したフォントハンドルを消してくれる便利な奴
 	InitFontToHandle();
 }
@@ -30,13 +31,10 @@ void FontHandle::Init()
 	//フォント名をNULLにすればデフォルトフォントになる
 
 	//登録
-	snoutCrayon_48_3 = CreateFontToHandle("殴り書きクレヨン", 48, 3);
-	natumemozi_38_8 = CreateFontToHandle("なつめもじ", 38, 8, DX_FONTTYPE_ANTIALIASING);
-	natumemozi_48_3 = CreateFontToHandle("なつめもじ", 48, 3);
-	natumemozi_48_8 = CreateFontToHandle("なつめもじ", 48, 8, DX_FONTTYPE_ANTIALIASING);
-	natumemozi_64_8 = CreateFontToHandle("なつめもじ", 64, 8, DX_FONTTYPE_ANTIALIASING);
+	natumemozi_38_8  = CreateFontToHandle("なつめもじ", 38, 8, DX_FONTTYPE_ANTIALIASING);
+	natumemozi_48_8  = CreateFontToHandle("なつめもじ", 48, 8, DX_FONTTYPE_ANTIALIASING);
+	natumemozi_64_8  = CreateFontToHandle("なつめもじ", 64, 8, DX_FONTTYPE_ANTIALIASING);
 	natumemozi_100_3 = CreateFontToHandle("なつめもじ", 100, 3, DX_FONTTYPE_ANTIALIASING);
-	weakForce_48_8 = CreateFontToHandle("851チカラヨワク", 48, 8, DX_FONTTYPE_ANTIALIASING);
 	weakForce_222_16 = CreateFontToHandle("851チカラヨワク", 222, 16, DX_FONTTYPE_ANTIALIASING);
 }
 
