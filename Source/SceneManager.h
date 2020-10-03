05cb2697e8cc8079c66e1879b86b7ca2c0ecd0a4
kuroneko96 <kaityou5555@gmail.com>
2020-10-04 03:29:18 +0900

    [add]現在のシーンを追加

--- Source/SceneManager.h
+++ Source/SceneManager.h
@@ -12,12 +12,16 @@ private:
 	std::stack<std::shared_ptr<BaseScene>> scenes;
 
 	bool isChange;
+	eScene nowScene;
+	eScene frontScene;
 public:
 	SceneManager();
 	void Update();
 	void Draw();
 	void SceneChange(eScene _nextScene, Parameter* _parameter,
 		const bool _isStack, const bool _isBack);
+
+	eScene GetNowScene() { return nowScene; }
 };
 
 #endif // !_SCENEMANAGER_H
