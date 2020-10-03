762dbbc15424b64f896ff8923892b3ca608ba7e3
kuroneko96 <kaityou5555@gmail.com>
2020-10-03 20:53:37 +0900

    [update]プレイヤーのサーチ処理を修正 [update]攻撃モーションの処理を関数化 [fix]咲夜スキル発動中アニメーションを行わないように修正 [fix]咲夜スキル発動中プレイヤーサーチを行わないように修正

--- Source/BaseEnemy.cpp
+++ Source/BaseEnemy.cpp
@@ -5,6 +5,7 @@
 
 #include "BasePlayer.h"
 #include "BulletManager.h"
+#include "CastleManager.h"
 #include "Image.h"
 
 const float BaseEnemy::ENEMY_SPAWNXLEFT = 420;
@@ -16,7 +17,14 @@ const float BaseEnemy::ENEMY_SPAWNYRIGHT = (GAME_HEIHGT / 2) - (48 / 2);
 const float BaseEnemy::ENEMY_SPAWNXDOWN = (GAME_WIDTH / 2) - (48 / 2);
 const float BaseEnemy::ENEMY_SPAWNYDOWN = GAME_HEIHGT + 48;
 
-//通常スポーン用のコンストラクタ
+/// <summary>
+/// 通常スポーン用のコンストラクタ
+/// </summary>
+/// <param name="_speed">スポーンするEnemyの速度</param>
+/// <param name="_power">スポーンするEnemyの攻撃力</param>
+/// <param name="_durability">スポーンするEnemyの耐久力</param>
+/// <param name="_direction">スポーンするEnemyの方向</param>
+/// <param name="_enemyType">スポーンするEnemyの種類</param>
 BaseEnemy::BaseEnemy(float _speed, float _power, int _durability,
 	eDirection _direction, eEnemyType _enemyType) {
 	//各パラメータの初期化
@@ -47,7 +55,16 @@ BaseEnemy::BaseEnemy(float _speed, float _power, int _durability,
 
 }
 
-//サブ拠点用のコンストラクタ
+/// <summary>
+/// サブ拠点スポーン用のコンストラクタ
+/// </summary>
+/// <param name="_x">スポーン位置x座標</param>
+/// <param name="_y">スポーン位置y座標</param>
+/// <param name="_speed">スポーンするEnemyの速度</param>
+/// <param name="_power">スポーンするEnemyの攻撃力</param>
+/// <param name="_durability">スポーンするEnemyの耐久力</param>
+/// <param name="_direction">スポーンするEnemyの方向</param>
+/// <param name="_enemyType">スポーンするEnemyの種類</param>
 BaseEnemy::BaseEnemy(float _x, float _y, float _speed, float _power, int _durability,
 	eDirection _direction, eEnemyType _enemyType) {
 	//各パラメータの初期化
@@ -59,6 +76,14 @@ BaseEnemy::BaseEnemy(float _x, float _y, float _speed, float _power, int _durabi
 	cy = y + (height / 2);
 }
 
+/// <summary>
+/// 共通の初期化項目の初期化処理
+/// </summary>
+/// <param name="_speed">スピードの初期値</param>
+/// <param name="_power">攻撃力の初期値</param>
+/// <param name="_durability">体力の初期値</param>
+/// <param name="_direction">方向の初期値</param>
+/// <param name="_enemyType">エネミーのタイプ情報</param>
 void BaseEnemy::Init(float _speed, float _power, int _durability,
 	eDirection _direction, eEnemyType _enemyType) {
 	speed = _speed;
@@ -70,11 +95,19 @@ void BaseEnemy::Init(float _speed, float _power, int _durability,
 	animationCnt = 0;
 	attackCnt = 0;
 	coolDownCnt = 0;
+	imageIndex = static_cast<int>(enemyType);
 
 	isActive = true;
 	isAttack = false;
 	isCoolDown = false;
 	isHit = false;
+
+	if (direction == eDirection::Left) {
+		isTurn = true;
+	}
+	else {
+		isTurn = false;
+	}
 
 	addX = 0;
 	addY = 0;
@@ -89,32 +122,136 @@ void BaseEnemy::Init(float _speed, float _power, int _durability,
 	height = 48;
 }
 
-//移動処理
-void BaseEnemy::Move() {
+/// <summary>
+/// 更新処理
+/// </summary>
+/// <param name="_castleManager">拠点の管理クラス</param>
+/// <param name="_player">プレイヤークラス</param>
+/// <param name="_bulletManager">弾の管理クラス</param>
+void BaseEnemy::Update(CastleManager* _castleManager, BasePlayer* _player,
+	BulletManager* _bulletManager) {
+/*------------------------------------------------------------------------------
+センター座標の再計算
+------------------------------------------------------------------------------*/
+	cx = x + (width / 2);
+	cy = y + (height / 2);
 
-	if (isActive == false || isAttack == true || inactiveType == eInactiveType::Invasion) {
+/*------------------------------------------------------------------------------
+攻撃タイプが侵入の際攻撃フラグをfalseに
+------------------------------------------------------------------------------*/
+	if (isAttack == true && attackType == eAttackType::Invasion) {
+		isAttack = false;
+	}
+
+/*------------------------------------------------------------------------------
+ダメージ処理
+------------------------------------------------------------------------------*/
+	for (int i = 0; i < _bulletManager->Get_MaxBullet(); i++) {
+		if (_bulletManager->Get_IsActive(i) == true) {
+			if (ClisionHit(x, y, width, height, _bulletManager->Get_X(i), _bulletManager->Get_Y(i),
+				_bulletManager->Get_Width(i), _bulletManager->Get_Height(i))) {
+				_bulletManager->Set_isActive(i, false);
+				DamageProc(_player->Get_power());
+			}
+		}
+	}
+/*------------------------------------------------------------------------------
+生存判定処理
+------------------------------------------------------------------------------*/
+	JudgeActive();
+
+/*------------------------------------------------------------------------------
+サーチ関連処理
+------------------------------------------------------------------------------*/
+
+	//クールダウンの処理
+	if (isCoolDown == true) {
+		SearchCoolDownTime();
+	}
+
+	//拠点のサーチ処理
+	for (int i = 0; i < _castleManager->Get_CastleNum(); i++) {
+		SearchCastle(_castleManager->Get_X(i), _castleManager->Get_Y(i),
+			_castleManager->Get_Width(i), _castleManager->Get_Height(i), _castleManager->Get_IsActive(i));
+	}
+
+	//プレイヤーのサーチ処理
+	SearchPlayer(_player->Get_x(), _player->Get_y(), _player->Get_width(), _player->Get_height(),
+		_player);
+
+/*------------------------------------------------------------------------------
+攻撃処理
+------------------------------------------------------------------------------*/
+	if (isAttack == true) {
+		AttackProc(_player->Get_cx(), _player->Get_cy());
+	}
+
+/*------------------------------------------------------------------------------
+移動処理
+------------------------------------------------------------------------------*/
+	Move(_player->Get_isAbility(), _player->Get_AbilityType());
+
+/*------------------------------------------------------------------------------
+アニメーション処理
+------------------------------------------------------------------------------*/
+	Animation(_player->Get_isAbility(), _player->Get_AbilityType());
+}
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void BaseEnemy::Draw() {
+/*------------------------------------------------------------------------------
+通常時＆攻撃時の描画処理
+------------------------------------------------------------------------------*/
+	if (inactiveType == eInactiveType::None) {
+		Image::Instance()->TransparentGraph(x, y, Image::Instance()->GetGraph(
+			eImageType::Gpicture_Enemy, imageIndex), 255, isTurn);
+	}
+/*------------------------------------------------------------------------------
+拠点侵入時の描画処理
+------------------------------------------------------------------------------*/
+	else if (inactiveType == eInactiveType::Invasion) {
+		animationCnt = Image::Instance()->FadeOutGraph(x, y, Image::Instance()->GetGraph(
+			eImageType::Gpicture_Enemy, imageIndex), animationCnt, 60, isTurn);
+	}
+}
+
+/// <summary>
+/// 移動処理
+/// </summary>
+/// <param name="_isAbility">プレイヤーのアビリティフラグ</param>
+/// <param name="_abilityType">プレイヤーのアビリティタイプ</param>
+void BaseEnemy::Move(bool _isAbility, int _abilityType) {
+	/*--------------------------------------------------------------
+	非アクティブの場合、攻撃中の場合、非アクティブタイプが侵入の場合
+	咲夜のスキルが発動中の場合、移動処理を行わない
+	--------------------------------------------------------------*/
+	if (isActive == false || isAttack == true || inactiveType == eInactiveType::Invasion ||
+		(_isAbility == true && _abilityType == SAKUYA)) {
 		return;
 	}
 
-	if (direction == eDirection::Left) {
+	//方向に対応する加算or減算処理おこなう
+	switch (direction) {
+	case eDirection::Left:		//左方向
 		x += speed;
-	}
-
-	if (direction == eDirection::Right) {
+		break;
+	case eDirection::Right:		//右方向
 		x -= speed;
-	}
-
-	if (direction == eDirection::Up) {
+		break;
+	case eDirection::Up:		//上方向
 		y += speed;
-	}
-
-	if (direction == eDirection::Down) {
+		break;
+	case eDirection::Down:		//下方向
 		y -= speed;
+		break;
 	}
-
 }
 
-//生存判定処理
+/// <summary>
+/// 生存判定処理
+/// </summary>
 void BaseEnemy::JudgeActive() {
 	//durabilityが0以下のとき
 	if (durability <= 0) {
@@ -128,36 +265,51 @@ void BaseEnemy::JudgeActive() {
 	}
 }
 
+/// <summary>
+/// 攻撃の処理
+/// </summary>
+/// <param name="_cx">プレイヤーのcx座標</param>
+/// <param name="_cy">プレイヤーのcy座標</param>
 void BaseEnemy::AttackProc(float _cx,float _cy) {
 
+	//攻撃タイプが侵入の場合処理を行わない
 	if (attackType == eAttackType::Invasion) {
 		return;
 	}
 
+	//攻撃モーションの初回処理
 	if (attackCnt == 0) {
+		/*--------------------------------------------------
+		1フレーム分のモーション移動距離を算出
+		x = (エネミーのcx - プレイヤーのcx) / 最大フレーム数
+		y = (エネミーのcy - プレイヤーのcy) / 最大フレーム数
+		※abs関数によって絶対値を返す
+		--------------------------------------------------*/
 		distanceToPlayerX = abs(cx - _cx) / 30.0f;
 		distanceToPlayerY = abs(cy - _cy) / 30.0f;
 
+		//モーション移動に使用する座標を割り当てる
 		switch (direction) {
-		case eDirection::Left:
+		case eDirection::Left:						//左方向
 			addX = distanceToPlayerX;
-			addY = -(ATTACK_ANIMATION_JUNPPOWER);
+			addY = -(ATTACK_MOTION_JUNPPOWER);
 			break;
-		case eDirection::Right:
+		case eDirection::Right:						//右方向
 			addX = -(distanceToPlayerX);
-			addY = -(ATTACK_ANIMATION_JUNPPOWER);
+			addY = -(ATTACK_MOTION_JUNPPOWER);
 			break;
-		case eDirection::Up:
-			addX = ATTACK_ANIMATION_JUNPPOWER;
+		case eDirection::Up:						//上方向
+			addX = ATTACK_MOTION_JUNPPOWER;
 			addY = distanceToPlayerY;
 			break;
-		case eDirection::Down:
-			addX = -(ATTACK_ANIMATION_JUNPPOWER);
+		case eDirection::Down:						//下方向
+			addX = -(ATTACK_MOTION_JUNPPOWER);
 			addY = -(distanceToPlayerY);
 			break;
 		}
 	}
 
+	//モーションの終了処理
 	if (attackCnt == 100) {
 		isAttack = false;
 		isCoolDown = true;
@@ -168,82 +320,81 @@ void BaseEnemy::AttackProc(float _cx,float _cy) {
 
 	//方向が左右の場合
 	if (direction == eDirection::Left || direction == eDirection::Right) {
-		if (attackCnt < 30) {
-			x += addX;
-
-			if (attackCnt < 15) {
-				y += addY;
-			}
-			else if (attackCnt < 30) {
-				y -= addY;
-			}
-
-		}
-		else if (attackCnt >= 50 && attackCnt < 80) {
-
-			x -= addX;
-
-			if (attackCnt < 65) {
-				y += addY;
-			}
-			else if (attackCnt < 80) {
-				y -= addY;
-			}
-		}
+		AttackMotion(&x, &y, addX, addY);
 	}
 	//方向が上下の場合
 	else if (direction == eDirection::Up || direction == eDirection::Down) {
-		if (attackCnt < 30) {
-			y += addY;
-
-			if (attackCnt < 15) {
-				x += addX;
-			}
-			else if (attackCnt < 30) {
-				x -= addX;
-			}
-
-		}
-		else if (attackCnt >= 50 && attackCnt < 80) {
-
-			y -= addY;
-
-			if (attackCnt < 65) {
-				x += addX;
-			}
-			else if (attackCnt < 80) {
-				x -= addX;
-			}
-		}
+		AttackMotion(&y, &x, addY, addX);
 	}
 
+	//攻撃カウントの加算処理
 	attackCnt++;
 }
 
+/// <summary>
+/// 攻撃モーション
+/// </summary>
+/// <param name="_move">移動する方向</param>
+/// <param name="_jump">ジャンプする方向</param>
+/// <param name="_addMove">加算させる移動値</param>
+/// <param name="_addJump">加算させるジャンプ値</param>
+void BaseEnemy::AttackMotion(float* _move, float* _jump, float _addMove, float _addJump) {
+	//プレイヤーの方向へ向かう処理
+	if (attackCnt < 30) {
+		*_move += _addMove;
+
+		if (attackCnt < 15) {
+			*_jump += _addJump;
+		}
+		else if (attackCnt < 30) {
+			*_jump -= _addJump;
+		}
+	}
+	//元の座標に戻る処理
+	else if (attackCnt >= 50 && attackCnt < 80) {
+		*_move -= _addMove;
+
+		if (attackCnt < 65) {
+			*_jump += _addJump;
+		}
+		else if (attackCnt < 80) {
+			*_jump -= _addJump;
+		}
+	}
+}
+
+/// <summary>
+/// ダメージ処理
+/// </summary>
+/// <param name="_damage">ダメージ量</param>
 void BaseEnemy::DamageProc(int _damage) {
 	durability -= _damage;
 }
 
-//プレイヤーサーチ処理
-/*------------------------------------------------------------------------------
-現状敵側は遠距離攻撃を想定してない判定式となってるので
-敵が遠距離攻撃を行うようにする際は判定式を変更する必要性がある
-------------------------------------------------------------------------------*/
+/// <summary>
+/// プレイヤーのサーチ処理
+/// </summary>
+/// <param name="_px">プレイヤーのx座標</param>
+/// <param name="_py">プレイヤーのy座標</param>
+/// <param name="_pw">プレイヤーの横幅</param>
+/// <param name="_ph">プレイヤーの縦幅</param>
+/// <param name="_player">プレイヤー</param>
 void BaseEnemy::SearchPlayer(float _px, float _py, float _pw, float _ph, BasePlayer* _player) {
 
-	if (_player->Get_isStan() || isAttack == true || isCoolDown == true) {
+	if (_player->Get_isStan() || isAttack == true || isCoolDown == true ||
+		(_player->Get_isAbility() == true && _player->Get_AbilityType() == SAKUYA)) {
 		return;
 	}
 
 	switch (direction) {
 	case eDirection::Left:
 		/*******************************************************************************
-		エネミーのx座標 + エネミーの横幅 + 参照する先の座標 <= プレイヤーのx座標 &&
-		エネミーのx座標 + 参照する先の座標 <= プレイヤーのx座標 + プレイヤーの横幅 &&
-		エネミーのy座標 + エネミーの縦幅 >= プレイヤーのy座標 &&
-		エネミーのy座標 <= プレイヤーのx座標 + プレイヤーの縦幅
+		エネミーのcx座標 + エネミーの横幅 >= プレイヤーのx座標 &&
+		エネミーのcx座標 <= プレイヤーのx座標 + プレイヤーの横幅の半分 &&
+		エネミーのy座標 + エネミーの縦幅 >= プレイヤーのy座標
+		エネミーのy座標 <= プレイヤーのy座標 + プレイヤーの横幅
 		*******************************************************************************/
-		if (x + (width * 2) >= _px && x + width <= _px + _pw &&
+		if (cx + width >= _px && cx <= _px + (_pw / 2) &&
 			y + height >= _py && y <= _py + _ph) {
 			isAttack = true;
 			attackType = eAttackType::Player;
@@ -251,12 +402,12 @@ void BaseEnemy::SearchPlayer(float _px, float _py, float _pw, float _ph, BasePla
 		break;
 	case eDirection::Right:
 		/*******************************************************************************
-		エネミーのx座標 + エネミーの横幅 <= プレイヤーのx座標 &&
-		エネミーのx座標 - 参照する先の座標 <= プレイヤーのx座標 + プレイヤーの横幅 &&
-		エネミーのy座標 + エネミーの縦幅 >= プレイヤーのy座標 &&
-		エネミーのy座標 <= プレイヤーのx座標 + プレイヤーの縦幅
+		エネミーのcx座標 >= プレイヤーのcx座標 &&
+		エネミーのcx座標 - エネミーの横幅 <= プレイヤーのcx + プレイヤーの横幅の半分 &&
+		エネミーのy座標 + エネミーの縦幅 >= プレイヤーのy座標
+		エネミーのy座標 <= プレイヤーのy座標 + プレイヤーの横幅
 		*******************************************************************************/
-		if (x + width >= _px && x - width <= _px + _pw &&
+		if (cx >= (_px + (_pw / 2)) && cx - width <= (_px + (_pw / 2)) + (_pw / 2) &&
 			y + height >= _py && y <= _py + _ph) {
 			isAttack = true;
 			attackType = eAttackType::Player;
@@ -270,7 +421,7 @@ void BaseEnemy::SearchPlayer(float _px, float _py, float _pw, float _ph, BasePla
 		エネミーのy座標 + 参照する先の座標 <= プレイヤーのx座標 + プレイヤーの縦幅
 		*******************************************************************************/
 		if (x + width >= _px && x <= _px + _pw &&
-			y + (height * 2) >= _py && y + height <= _py + _ph) {
+			cy + height >= _py && cy <= _py + (_ph / 2)) {
 			isAttack = true;
 			attackType = eAttackType::Player;
 		}
@@ -283,7 +434,7 @@ void BaseEnemy::SearchPlayer(float _px, float _py, float _pw, float _ph, BasePla
 		エネミーのy座標 + エネミーの縦幅 <= プレイヤーのx座標 - 参照する先の座標
 		*******************************************************************************/
 		if (x + width >= _px && x <= _px + _pw &&
-			y + height >= _py && y - height <= _py + _ph) {
+			cy >= (_py + (_ph / 2)) && cy - height <= (_py + (_ph / 2)) + (_ph / 2)) {
 			isAttack = true;
 			attackType = eAttackType::Player;
 		}
@@ -291,7 +442,14 @@ void BaseEnemy::SearchPlayer(float _px, float _py, float _pw, float _ph, BasePla
 	}
 }
 
-//城サーチ処理
+/// <summary>
+/// 拠点のサーチ処理
+/// </summary>
+/// <param name="_ox">拠点のx座標</param>
+/// <param name="_oy">拠点のy座標</param>
+/// <param name="_ow">拠点の縦幅</param>
+/// <param name="_oh">拠点の縦幅</param>
+/// <param name="_isActive">拠点のアクティブフラグ</param>
 void BaseEnemy::SearchCastle(float _ox, float _oy, float _ow, float _oh, bool _isActive) {
 
 	//非アクティブタイプが侵入の場合処理を行わない
@@ -309,7 +467,9 @@ void BaseEnemy::SearchCastle(float _ox, float _oy, float _ow, float _oh, bool _i
 	}
 }
 
-//クールダウン処理
+/// <summary>
+/// サーチのクールダウン
+/// </summary>
 void BaseEnemy::SearchCoolDownTime() {
 	//180フレーム目クールダウンを終了する
 	if (coolDownCnt == 180) {
@@ -321,7 +481,18 @@ void BaseEnemy::SearchCoolDownTime() {
 	coolDownCnt++;
 }
 
-//当たり判定処理
+/// <summary>
+/// あたり判定処理
+/// </summary>
+/// <param name="mx">エネミーのx座標</param>
+/// <param name="my">エネミーのy座標</param>
+/// <param name="mw">エネミーの横幅</param>
+/// <param name="mh">エネミーの縦幅</param>
+/// <param name="ox">相手のx座標</param>
+/// <param name="oy">相手のy座標</param>
+/// <param name="ow">相手の横幅</param>
+/// <param name="oh">相手の縦幅</param>
+/// <returns>当たった場合 true / 当たらなかった場合 false</returns>
 bool BaseEnemy::ClisionHit(float mx, float my, float mw, float mh,
 	float ox, float oy, float ow, float oh) {
 
@@ -332,14 +503,12 @@ bool BaseEnemy::ClisionHit(float mx, float my, float mw, float mh,
 	return false;
 }
 
-void BaseEnemy::Animation() {
-	int imageIndex = static_cast<int>(enemyType);	//どの画像を呼び出すかの変数
-	bool isTurn = false;							//LR反転フラグ
-
-	if (direction == eDirection::Left && isTurn != true) {
-		isTurn = true;
-	}
-
+/// <summary>
+/// 移動処理
+/// </summary>
+/// <param name="_isAbility">プレイヤーのアビリティフラグ</param>
+/// <param name="_abilityType">プレイヤーのアビリティタイプ</param>
+void BaseEnemy::Animation(bool _isAbility, int _abilityType) {
 	//非アクティブタイプがNoneの場合
 	if (inactiveType == eInactiveType::None) {
 		//カウントが60の時リセットする
@@ -356,21 +525,29 @@ void BaseEnemy::Animation() {
 			imageIndex = static_cast<int>(enemyType) + 1;
 		}
 
-		Image::Instance()->TransparentGraph(x, y, Image::Instance()->GetGraph(
-			eImageType::Gpicture_Enemy, imageIndex), 255, isTurn);
-
 		//エネミーが非アクティブまたは攻撃中の際、animationCutを加算しない
-		if (isActive == false || isAttack == true) {
+		if (isActive == false || isAttack == true ||
+			(_isAbility == true && _abilityType == SAKUYA)) {
 			return;
 		}
 
 		animationCnt++;
 	}
-	//非アクティブタイプが侵入の場合
-	else if (inactiveType == eInactiveType::Invasion) {
-		animationCnt = Image::Instance()->FadeOutGraph(x, y, Image::Instance()->GetGraph(
-			eImageType::Gpicture_Enemy, imageIndex), animationCnt, 60, isTurn);
-	}
+}
 
-	DrawBoxAA(x, y, x + width, y + height, GetColor(255, 255, 255), FALSE, 3);
+/// <summary>
+/// directionを設定する
+/// </summary>
+/// <param name="_direction">設定する方向</param>
+void BaseEnemy::SetDirection(eDirection _direction) {
+	//方向を設定する
+	direction = _direction;
+
+	//LR反転フラグも再設定する
+	if (direction == eDirection::Left) {
+		isTurn = true;
+	}
+	else {
+		isTurn = false;
+	}
 }
--- Source/BaseEnemy.h
+++ Source/BaseEnemy.h
@@ -49,10 +49,9 @@ protected:
 	static const float ENEMY_SPAWNXDOWN;	//エネミーの初期位置X下
 	static const float ENEMY_SPAWNYDOWN;	//エネミーの初期位置Y下
 
-	const int ANIMATION_SWITCHING = 30;
+	const int ANIMATION_SWITCHING = 30;		//アニメーションの切り替えフラグ
 
-	const float ATTACK_ANIMATION_MOVEAMOUNT	= 48.0f / 30.0f;
-	const float ATTACK_ANIMATION_JUNPPOWER	= 15.0f / 15.0f;
+	const float ATTACK_MOTION_JUNPPOWER		= 15.0f / 15.0f;	//上方向へのジャンプ力
 
 	float speed;			//エネミーの移動速度
 	float power;			//エネミーの攻撃力
@@ -64,15 +63,18 @@ protected:
 	int attackCnt;		//攻撃中のカウンタ
 	int coolDownCnt;	//クールダウンカウンタ
 
+	int imageIndex;		//描画指定変数
+	bool isTurn;		//LR反転フラグ
+
 	bool isActive;		//エネミーの生存フラグ
 	bool isAttack;		//エネミーの攻撃フラグ
 	bool isCoolDown;	//クールダウンフラグ
 
-	float addX;
-	float addY;
+	float addX;			//攻撃モーション用の加算x
+	float addY;			//攻撃モーション用の加算y
 
-	float distanceToPlayerX;
-	float distanceToPlayerY;
+	float distanceToPlayerX;	//プレイヤーとエネミーの中心座標間の距離x
+	float distanceToPlayerY;	//プレイヤーとエネミーの中心座標間の距離y
 
 	eAttackType attackType;		//攻撃のタイプ
 	eInactiveType inactiveType;	//非アクティブのタイプ
@@ -91,55 +93,245 @@ protected:
 	/// <summary>
 	/// 移動処理
 	/// </summary>
-	void Move();
+	/// <param name="_isAbility">プレイヤーのアビリティフラグ</param>
+	/// <param name="_abilityType">プレイヤーのアビリティタイプ</param>
+	void Move(bool _isAbility, int _abilityType);
 
 	/// <summary>
-	/// アニメーション
+	/// 生存判定処理
 	/// </summary>
-	void Animation();
+	void JudgeActive();
+
+	/// <summary>
+	/// 攻撃の処理
+	/// </summary>
+	/// <param name="_cx">プレイヤーのcx座標</param>
+	/// <param name="_cy">プレイヤーのcy座標</param>
+	void AttackProc(float _cx, float _cy);
+
+	/// <summary>
+	/// 攻撃モーション
+	/// </summary>
+	/// <param name="_move">移動する方向</param>
+	/// <param name="_jump">ジャンプする方向</param>
+	/// <param name="_addMove">加算させる移動値</param>
+	/// <param name="_addJump">加算させるジャンプ値</param>
+	void AttackMotion(float* _move, float* _jump, float _addMove, float _addJump);
+
+	/// <summary>
+	/// ダメージ処理
+	/// </summary>
+	/// <param name="_damage">ダメージ量</param>
+	void DamageProc(int _damage);
+
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	/// <param name="_isAbility">プレイヤーのアビリティフラグ</param>
+	/// <param name="_abilityType">プレイヤーのアビリティタイプ</param>
+	void Animation(bool _isAbility, int _abilityType);
+
+	/// <summary>
+	/// プレイヤーのサーチ処理
+	/// </summary>
+	/// <param name="_px">プレイヤーのx座標</param>
+	/// <param name="_py">プレイヤーのy座標</param>
+	/// <param name="_pw">プレイヤーの横幅</param>
+	/// <param name="_ph">プレイヤーの縦幅</param>
+	/// <param name="_player">プレイヤー</param>
+	void SearchPlayer(float _px, float _py, float _pw, float _ph, BasePlayer* _player);
+
+	/// <summary>
+	/// サーチのクールダウン
+	/// </summary>
+	void SearchCoolDownTime();
+
+	/// <summary>
+	/// 拠点のサーチ処理
+	/// </summary>
+	/// <param name="_ox">拠点のx座標</param>
+	/// <param name="_oy">拠点のy座標</param>
+	/// <param name="_ow">拠点の縦幅</param>
+	/// <param name="_oh">拠点の縦幅</param>
+	/// <param name="_isActive">拠点のアクティブフラグ</param>
+	void SearchCastle(float _ox, float _oy, float _ow, float _oh, bool _isActive);
 public:
+	/// <summary>
+	/// デフォルトコンストラクタ
+	/// </summary>
 	BaseEnemy(){}
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	virtual ~BaseEnemy(){}
+
+	/// <summary>
+	/// 通常スポーン用のコンストラクタ
+	/// </summary>
+	/// <param name="_speed">スポーンするEnemyの速度</param>
+	/// <param name="_power">スポーンするEnemyの攻撃力</param>
+	/// <param name="_durability">スポーンするEnemyの耐久力</param>
+	/// <param name="_direction">スポーンするEnemyの方向</param>
+	/// <param name="_enemyType">スポーンするEnemyの種類</param>
 	BaseEnemy(float _speed, float _power, int _durability,
 		eDirection _direction, eEnemyType _enemyType);
+
+	/// <summary>
+	/// サブ拠点スポーン用のコンストラクタ
+	/// </summary>
+	/// <param name="_x">スポーン位置x座標</param>
+	/// <param name="_y">スポーン位置y座標</param>
+	/// <param name="_speed">スポーンするEnemyの速度</param>
+	/// <param name="_power">スポーンするEnemyの攻撃力</param>
+	/// <param name="_durability">スポーンするEnemyの耐久力</param>
+	/// <param name="_direction">スポーンするEnemyの方向</param>
+	/// <param name="_enemyType">スポーンするEnemyの種類</param>
 	BaseEnemy(float _x, float _y, float _speed, float _power, int _durability,
 		eDirection _direction, eEnemyType _enemyType);
 
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	/// <param name="_castleManager">拠点の管理クラス</param>
+	/// <param name="_player">プレイヤークラス</param>
+	/// <param name="_bulletManager">弾の管理クラス</param>
 	virtual void Update(CastleManager* _castleManager, BasePlayer* _player,
-		BulletManager* _bulletManager) {}							//更新処理
-	virtual void Draw() {}											//描画処理
+		BulletManager* _bulletManager);
 
-	void SearchCastle(float _ox, float _oy, float _ow, float _oh, bool _isActive);	//城の範囲内かサーチ
-	void SearchPlayer(float _px, float _py, float _pw, float _ph,
-		BasePlayer* _player);										//プレイヤーが攻撃範囲内かサーチ
-	void SearchCoolDownTime();				//サーチのクールダウン
-	void JudgeActive();						//アクティブかを判断する
-	void AttackProc(float _cx, float _cy);	//攻撃中の時間処理
-	void DamageProc(int _damage);			//ダメージ処理
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	virtual void Draw();
 
-	void Set_X(float _x) { x = _x; }										//x座標を設定する
-	void Set_Y(float _y) { y = _y; }										//y座標を設定する
-	void Set_Width(float _width) { width = _width; }						//widthを設定する
-	void Set_Height(float _height) { height = _height; }					//heightを設定する
-	void SetIsAttack(bool _isAttack) { isAttack = _isAttack; }				//isAttackを設定する
-	void SetIsActive(bool _isActive) { isActive = _isActive; }				//isActiveを設定する
-	void SetDirection(eDirection _direction) { direction = _direction; }	//directionを設定する
+	/// <summary>
+	/// x座標を設定する
+	/// </summary>
+	/// <param name="_x">設定するx座標</param>
+	void Set_X(float _x) { x = _x; }
 
-	float Get_X() { return x; }									//x座標を取得する
-	float Get_Y() { return y; }									//y座標を取得する
-	float Get_CX() { return cx; }								//cx座標を取得する
-	float Get_CY() { return cy; }								//cy座標を取得する
-	float Get_Width() { return width; }							//widthを取得する
-	float Get_Height() { return height; }						//heightを取得する
-	float GetPower() { return power; }							//エネミーの攻撃力を取得する
-	bool GetIsActive() { return isActive; }						//生存フラグを取得する
-	bool GetIsAttack() { return isAttack; }						//アタックフラグを取得する
-	eDirection GetDirection() { return direction; }				//方向を取得する
-	eInactiveType GetInactiveType() { return inactiveType; }	//非アクティブのタイプを取得する
-	eAttackType GetAttackType() { return attackType; }			//攻撃タイプを取得する
+	/// <summary>
+	/// y座標を設定する
+	/// </summary>
+	/// <param name="_y">設定するy座標</param>
+	void Set_Y(float _y) { y = _y; }
+
+	/// <summary>
+	/// widthを設定する
+	/// </summary>
+	/// <param name="_width">設定する横幅</param>
+	void Set_Width(float _width) { width = _width; }
+
+	/// <summary>
+	/// heightを設定する
+	/// </summary>
+	/// <param name="_height">設定する縦幅</param>
+	void Set_Height(float _height) { height = _height; }
+
+	/// <summary>
+	/// isAttackを設定する
+	/// </summary>
+	/// <param name="_isAttack">設定する攻撃フラグ</param>
+	void SetIsAttack(bool _isAttack) { isAttack = _isAttack; }
+
+	/// <summary>
+	/// isActiveを設定する
+	/// </summary>
+	/// <param name="_isActive">設定するアクティブフラグ</param>
+	void SetIsActive(bool _isActive) { isActive = _isActive; }
+
+	/// <summary>
+	/// directionを設定する
+	/// </summary>
+	/// <param name="_direction">設定する方向</param>
+	void SetDirection(eDirection _direction);
+
+	/// <summary>
+	/// x座標を取得する
+	/// </summary>
+	/// <returns>x座標</returns>
+	float Get_X() { return x; }
+
+	/// <summary>
+	/// y座標を取得する
+	/// </summary>
+	/// <returns>y座標</returns>
+	float Get_Y() { return y; }
+
+	/// <summary>
+	/// cx座標を取得する
+	/// </summary>
+	/// <returns>cx座標</returns>
+	float Get_CX() { return cx; }
+
+	/// <summary>
+	/// cy座標を取得する
+	/// </summary>
+	/// <returns>cy座標</returns>
+	float Get_CY() { return cy; }
+
+	/// <summary>
+	/// widthを取得する
+	/// </summary>
+	/// <returns>width</returns>
+	float Get_Width() { return width; }
+
+	/// <summary>
+	/// heightを取得する
+	/// </summary>
+	/// <returns>height</returns>
+	float Get_Height() { return height; }
+
+	/// <summary>
+	/// エネミーの攻撃力を取得する
+	/// </summary>
+	/// <returns>エネミーの攻撃力</returns>
+	float GetPower() { return power; }
+
+	/// <summary>
+	/// 生存フラグを取得する
+	/// </summary>
+	/// <returns>生存フラグ</returns>
+	bool GetIsActive() { return isActive; }
+
+	/// <summary>
+	/// アタックフラグを取得する
+	/// </summary>
+	/// <returns>アタックフラグ</returns>
+	bool GetIsAttack() { return isAttack; }
+
+	/// <summary>
+	/// 方向を取得する
+	/// </summary>
+	/// <returns>方向</returns>
+	eDirection GetDirection() { return direction; }
+
+	/// <summary>
+	/// 非アクティブのタイプを取得する
+	/// </summary>
+	/// <returns>非アクティブのタイプ</returns>
+	eInactiveType GetInactiveType() { return inactiveType; }
+
+	/// <summary>
+	/// 攻撃タイプを取得する
+	/// </summary>
+	/// <returns>攻撃タイプ</returns>
+	eAttackType GetAttackType() { return attackType; }
 	
+	/// <summary>
+	/// あたり判定処理
+	/// </summary>
+	/// <param name="mx">エネミーのx座標</param>
+	/// <param name="my">エネミーのy座標</param>
+	/// <param name="mw">エネミーの横幅</param>
+	/// <param name="mh">エネミーの縦幅</param>
+	/// <param name="ox">相手のx座標</param>
+	/// <param name="oy">相手のy座標</param>
+	/// <param name="ow">相手の横幅</param>
+	/// <param name="oh">相手の縦幅</param>
+	/// <returns>当たった場合 true / 当たらなかった場合 false</returns>
 	bool ClisionHit(float mx, float my, float mw, float mh,
-		float ox, float oy, float ow, float oh);			// m = my 自分   o = opnet 相手	//当たり判定処理
+		float ox, float oy, float ow, float oh);
 };
 
 #endif _BASEENEMY_H
