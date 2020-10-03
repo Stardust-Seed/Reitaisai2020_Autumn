5b0db2565f7701cbab6edb521f9c18147f7550cd
dooooori <binamu1215@gmail.com>
2020-10-04 00:58:00 +0900

    [add] 百から一の位のゲッターを作成(UI側で表示する時用)

--- Source/TimeLimit.cpp
+++ Source/TimeLimit.cpp
@@ -4,14 +4,20 @@
 
 TimeLimit::TimeLimit() {
 	timer = MAX_LIMITTIME;						//制限時間のセット
+	time = timer / FRAME;						//初期化
 
 	color = GetColor(255, 255, 255);			//文字の色　白
+
+	hundredsPlace = 0;							//初期化
+	tensPlace = 0;								//初期化
+	onesPlace = 0;								//初期化
 
 	finishTime = false;							//初期状態
 }
 
 void TimeLimit::Update() {
 	Draw();											//描画
+	time = timer / FRAME;							//人間用タイマー更新
 	if (timer <= FRAME) {								//残り一秒以下は割り算の結果0になるため
 		finishTime = true;							//フラグ切替
 	}
@@ -21,12 +27,31 @@ void TimeLimit::Update() {
 }
 
 void TimeLimit::Draw() {
-	//DrawFormatString(10, 60,color, "制限時間%d",timer/FRAME);	//表示
 	DrawFormatStringToHandle(10, 60, color, FontHandle::Instance()->Get_natumemozi_38_8(), "TIME :%d", timer / FRAME);
+	//DrawFormatStringToHandle(150, 60, color, FontHandle::Instance()->Get_natumemozi_38_8(), "100:%d", Get_hundredsPlace());
+	//DrawFormatStringToHandle(250, 60, color, FontHandle::Instance()->Get_natumemozi_38_8(), "10:%d", Get_tensPlace());
+	//DrawFormatStringToHandle(350, 60, color, FontHandle::Instance()->Get_natumemozi_38_8(), "1:%d", Get_onesPlace());
 } 
 
 void TimeLimit::CountDown() {
-	timer -= 1;												//カウントダウン
+	timer -= 1;						//カウントダウン
+}
+
+int TimeLimit::Get_hundredsPlace() {
+	hundredsPlace = time / 100;				
+	return hundredsPlace;			//百の位
+}
+
+int TimeLimit::Get_tensPlace() {
+	tensPlace = time - (Get_hundredsPlace() * 100);	//百の位を引く
+	tensPlace = tensPlace / 10;				//十の位のみ
+	return tensPlace;
+}
+
+int TimeLimit::Get_onesPlace() {
+	onesPlace = time - (Get_hundredsPlace() * 100);	//百の位を引く
+	onesPlace = onesPlace - (Get_tensPlace() * 10);//十の位を引く
+	return onesPlace;
 }
 
 bool TimeLimit::Get_finishTime() {
--- Source/TimeLimit.h
+++ Source/TimeLimit.h
@@ -9,9 +9,17 @@ private:
 
 	static const int MAX_LIMITTIME = FRAME * (TIME + 1)	-1;	// フレームレート * (指定した秒数 +1) - 1 // "+1) - 1"この部分は表示の違和感をなくすため
 
-	int timer;						//制限時間(減少表示)
+	int timer;						//制限時間(機械用)
 	
+	int time;						//制限時間(人間用)
+
 	int color;						//テキストカラー
+
+	int hundredsPlace;				//百の位
+
+	int tensPlace;					//十の位
+
+	int onesPlace;					//一の位
 
 	bool finishTime;				//終了フラグ
 
@@ -23,6 +31,12 @@ public:
 	void Draw();					//描画処理
 
 	void CountDown();				//カウントダウン
+
+	int Get_hundredsPlace();			//百の位のゲッター
+
+	int Get_tensPlace();			//十の位のゲッター
+
+	int Get_onesPlace();			//一の位のゲッター
 
 	bool Get_finishTime();			//終了フラグのゲッター
 };
